17a6cffec94970228dd4716ae9acd104dc26b22d
Andrej Ikica <andrej@epsilonlabs.ai>
2024-12-06 12:14:56 +0000

    Logger changes.

--- old_repos/epsutils/epsutils/logging/logging_utils.py
+++ old_repos/epsutils/epsutils/logging/logging_utils.py
@@ -1,10 +1,10 @@
 import logging
 
 
-def configure_logger(logger_file_name, show_logging_level=True):
+def configure_logger(logger_file_name, logging_level=logging.INFO, show_logging_level=True):
     logging.basicConfig(
         filename=logger_file_name,
         filemode="w",
         format="%(levelname)s: %(message)s" if show_logging_level else "%(message)s",
         datefmt="%y-%m-%d %H:%M:%S",
-        level=logging.INFO)
+        level=logging_level)
