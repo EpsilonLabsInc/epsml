31384893d688e498d19431ed1ce67889166a8b4f
Andrej Ikica <andrej@epsilonlabs.ai>
2024-12-08 19:57:18 +0000

    Added option to append to existing log file.

--- old_repos/epsutils/epsutils/logging/logging_utils.py
+++ old_repos/epsutils/epsutils/logging/logging_utils.py
@@ -1,10 +1,10 @@
 import logging
 
 
-def configure_logger(logger_file_name, logging_level=logging.INFO, show_logging_level=True):
+def configure_logger(logger_file_name, logging_level=logging.INFO, show_logging_level=True, append_to_existing_log_file=False):
     logging.basicConfig(
         filename=logger_file_name,
-        filemode="w",
+        filemode="a" if append_to_existing_log_file else "w",
         format="%(levelname)s: %(message)s" if show_logging_level else "%(message)s",
         datefmt="%y-%m-%d %H:%M:%S",
         level=logging_level)
