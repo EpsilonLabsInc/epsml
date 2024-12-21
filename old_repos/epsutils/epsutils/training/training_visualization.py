963522814cd444266f3fd03d3c5f9f6dfdf3e12d
Andrej Ikica <andrej@epsilonlabs.ai>
2024-12-21 09:55:48 +0000

    Show validation results.

---
+++ old_repos/epsutils/epsutils/training/training_visualization.py
@@ -0,0 +1,31 @@
+import math
+
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+
+def show_training_data(training_data, num_view_grid_columns, label_to_string_mapping):
+    inputs = training_data["inputs"]
+    labels = [label_to_string_mapping[label.item()] for label in training_data["labels"]]
+    probabilities = [probability.item() for probability in training_data["probabilities"]] if "probabilities" in training_data else None
+
+    NUM_IMAGES = inputs.size(0)
+    NUM_ROWS = math.ceil(NUM_IMAGES / num_view_grid_columns)
+
+    clear_output(wait=True)
+    plt.figure(figsize=(num_view_grid_columns * 2, NUM_ROWS * 2))
+
+    for i in range(NUM_IMAGES):
+        plt.subplot(NUM_ROWS, num_view_grid_columns, i + 1)
+        plt.imshow(inputs[i, 0, :, :].cpu().numpy(), cmap="gray")
+
+        title = f"Label: {labels[i]}"
+
+        if probabilities is not None:
+            title += f" ({probabilities[i]:.2f})"
+
+        plt.title(title, fontsize=12)
+        plt.axis("off")
+
+    plt.tight_layout()
+    plt.show()
