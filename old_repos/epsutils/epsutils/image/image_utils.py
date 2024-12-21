31c4e79a1e215f7c4395c8158cc423a6c766cf67
Andrej Ikica <andrej@epsilonlab.ai>
2024-12-21 08:04:21 +0000

    Fixed syntax error.

--- old_repos/epsutils/epsutils/image/image_utils.py
+++ old_repos/epsutils/epsutils/image/image_utils.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 
-def validate_image_histogram(image, lateral_entropy_min=5.5, lateral_entropy_max=8.0, ap_entropy_min=5.25, ap_entropy_max=7.75)
+def validate_image_histogram(image, lateral_entropy_min=5.5, lateral_entropy_max=8.0, ap_entropy_min=5.25, ap_entropy_max=7.75):
     try:
         pixrange = image.getextrema()[1] - image.getextrema()[0]
 
