5671eae8e8c8d8ec4358b9eff42dc4e2f77d620e
Andrej Ikica <andrej@epsilonlabs.ai>
2024-12-22 18:19:08 +0000

    Renaming #3.

---
+++ old_repos/epsclassifiers/epsclassifiers/cr_chest_classifier/cr_chest_classifier.py
@@ -0,0 +1,9 @@
+import torchxrayvision as xrv
+
+
+class CrChestClassifier:
+    def __init__():
+        self.__model = xrv.models.DenseNet(num_classes=1)
+
+    def model(self):
+        return self.__model
