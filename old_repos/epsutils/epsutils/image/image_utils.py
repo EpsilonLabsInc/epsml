3837c384fa4ec0a4320847b082752cb15d810ba8
Andrej Ikica <andrej@epsilonlabs.ai>
2024-12-30 19:54:51 +0000

    Added image validation configs.

--- old_repos/epsutils/epsutils/image/image_utils.py
+++ old_repos/epsutils/epsutils/image/image_utils.py
@@ -3,7 +3,28 @@ from typing import Optional
 import numpy as np
 
 
-def validate_image_histogram(image, lateral_entropy_min=5.5, lateral_entropy_max=8.0, ap_entropy_min=5.25, ap_entropy_max=7.75):
+VALIDATE_IMAGE_HISTOGRAM_CONFIGURATIONS = {
+    "CHEST_CR_SCAN": {
+        "lateral_entropy_min": 5.5,
+        "lateral_entropy_max": 8.0,
+        "ap_entropy_min": 5.25,
+        "ap_entropy_max": 7.75
+    },
+    "NON_CHEST_CR_SCAN": {
+        "lateral_entropy_min": 5.25,
+        "lateral_entropy_max": 8.0,
+        "ap_entropy_min": 5.25,
+        "ap_entropy_max": 8.0
+    }
+}
+
+
+def validate_image_histogram(image, config=VALIDATE_IMAGE_HISTOGRAM_CONFIGURATIONS["CHEST_CR_SCAN"]):
+    lateral_entropy_min = config["lateral_entropy_min"]
+    lateral_entropy_max = config["lateral_entropy_max"]
+    ap_entropy_min = config["ap_entropy_min"]
+    ap_entropy_max = config["ap_entropy_max"]
+
     try:
         pixrange = image.getextrema()[1] - image.getextrema()[0]
 
