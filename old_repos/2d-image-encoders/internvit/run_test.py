6298f2fad6b9cd50a4bb942d2ef9430491841105
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-16 17:39:51 +0000

    Added InternVIT test script.

---
+++ old_repos/2d-image-encoders/internvit/run_test.py
@@ -0,0 +1,39 @@
+import torch
+from PIL import Image
+from transformers import AutoModel, CLIPImageProcessor
+
+from epsutils.dicom import dicom_utils
+
+MODEL_PATH = "OpenGVLab/InternViT-300M-448px-V2_5"
+IMAGE_PATH = "./sample.dcm"
+
+
+def main():
+    # Create the model.
+    model = AutoModel.from_pretrained(
+        MODEL_PATH,
+        torch_dtype=torch.bfloat16,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True).cuda().eval()
+
+    # Create image preprocessor.
+    image_processor = CLIPImageProcessor.from_pretrained(MODEL_PATH)
+
+    # Preprocess image.
+    image = dicom_utils.get_dicom_image(IMAGE_PATH, custom_windowing_parameters={"window_center": 0, "window_width": 0})
+    image = Image.fromarray(image).convert("RGB")
+    pixel_values = image_processor(images=image, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(torch.bfloat16).cuda()
+
+    # Run inference.
+    output = model(pixel_values)
+
+    print(f"Input tensor size: {pixel_values.shape}")
+    print("Output:")
+    print(output)
+    print(f"Output type: {type(output)}")
+    print(f"Output size: {output.pooler_output.shape}")
+
+
+if __name__ == "__main__":
+    main()
