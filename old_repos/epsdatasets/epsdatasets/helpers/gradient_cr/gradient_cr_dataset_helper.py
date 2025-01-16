bf2c3c63ddcf816e0739cd8e0321c1df97fa08b0
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-16 22:59:18 +0000

    Added Gradient CR dataset helper.

---
+++ old_repos/epsdatasets/epsdatasets/helpers/gradient_cr/gradient_cr_dataset_helper.py
@@ -0,0 +1,208 @@
+import ast
+import os
+
+import datasets
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+
+from epsdatasets.helpers.base.base_dataset_helper import BaseDatasetHelper
+from epsutils.gcs import gcs_utils
+from epsutils.labels import labels_utils
+from epsutils.labels.cr_chest_labels import CR_CHEST_LABELS
+
+
+class GradientCrDatasetHelper(BaseDatasetHelper):
+    def __init__(self, gcs_train_file, gcs_validation_file, gcs_test_file=None, images_dir=None):
+        """
+        Initializes the GradientCrDatasetHelper with the specified parameters.
+
+        Args:
+            gcs_train_file (str): GCS URI of the training file.
+            gcs_validation_file (str): GCS URI of the validation file.
+            gcs_test_file (str, optional): GCS URI of the test file.
+            images_dir (str, optional): GCS URI or local path of the base folder where the images are located.
+        """
+        super().__init__(gcs_train_file=gcs_train_file,
+                         gcs_validation_file=gcs_validation_file,
+                         gcs_test_file=gcs_test_file,
+                         images_dir=images_dir)
+
+    def _load_dataset(self, *args, **kwargs):
+        # Store params.
+        self.__gcs_train_file = kwargs["gcs_train_file"] if "gcs_train_file" in kwargs else next((arg for arg in args if arg == "gcs_train_file"), None)
+        self.__gcs_validation_file = kwargs["gcs_validation_file"] if "gcs_validation_file" in kwargs else next((arg for arg in args if arg == "gcs_validation_file"), None)
+        self.__gcs_test_file = kwargs["gcs_test_file"] if "gcs_test_file" in kwargs else next((arg for arg in args if arg == "gcs_test_file"), None)
+        self.__images_dir = kwargs["images_dir"] if "images_dir" in kwargs else next((arg for arg in args if arg == "images_dir"), None)
+
+        self.__pandas_full_dataset = None
+        self.__pandas_train_dataset = None
+        self.__pandas_validation_dataset = None
+        self.__pandas_test_dataset = None
+        self.__torch_train_dataset = None
+        self.__torch_validation_dataset = None
+        self.__torch_test_dataset = None
+
+        # Download training file.
+        print("Downloading the training file")
+        gcs_data = gcs_utils.split_gcs_uri(self.__gcs_train_file)
+        content = gcs_utils.download_file_as_string(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+
+        # Populate training data.
+        print("Populating the training data")
+        data = []
+        rows = content.splitlines()
+        for row in rows:
+            row = ast.literal_eval(row)
+            labels = row["labels"]
+            images = row["image"]
+
+            for image in images:
+                image_path = os.path.join(self.__images_dir, image) if self.__images_dir else image
+                data.append({"image_path": image_path, "labels": labels})
+
+        # Create traning dataset.
+        print("Creating the training dataset")
+        self.__pandas_train_dataset = pd.DataFrame(data)
+
+        # Print training dataset.
+        print(f"The training dataset has {len(self.__pandas_train_dataset)} rows")
+        print("Training dataset head:")
+        print(self.__pandas_train_dataset.head())
+
+        # Download validation file.
+        print("Downloading the validation file")
+        gcs_data = gcs_utils.split_gcs_uri(self.__gcs_validation_file)
+        content = gcs_utils.download_file_as_string(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+
+        # Populate validation data.
+        print("Populating the validation data")
+        data = []
+        rows = content.splitlines()
+        for row in rows:
+            row = ast.literal_eval(row)
+            labels = row["labels"]
+            images = row["image"]
+
+            for image in images:
+                image_path = os.path.join(self.__images_dir, image) if self.__images_dir else image
+                data.append({"image_path": image_path, "labels": labels})
+
+        # Create validation dataset.
+        print("Creating the validation dataset")
+        self.__pandas_validation_dataset = pd.DataFrame(data)
+
+        # Print validation dataset.
+        print(f"The validation dataset has {len(self.__pandas_validation_dataset)} rows")
+        print("Validation dataset head:")
+        print(self.__pandas_validation_dataset.head())
+
+        if self.__gcs_test_file is not None:
+            # Download test file.
+            print("Downloading the test file")
+            gcs_data = gcs_utils.split_gcs_uri(self.__gcs_test_file)
+            content = gcs_utils.download_file_as_string(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+
+            # Populate test data.
+            print("Populating the test data")
+            data = []
+            rows = content.splitlines()
+            for row in rows:
+                row = ast.literal_eval(row)
+                labels = row["labels"]
+                images = row["image"]
+
+                for image in images:
+                    image_path = os.path.join(self.__images_dir, image) if self.__images_dir else image
+                    data.append({"image_path": image_path, "labels": labels})
+
+            # Create test dataset.
+            print("Creating the test dataset")
+            self.__pandas_test_dataset = pd.DataFrame(data)
+
+            # Print test dataset.
+            print(f"The test dataset has {len(self.__pandas_test_dataset)} rows")
+            print("Test dataset head:")
+            print(self.__pandas_test_dataset.head())
+
+        # Create Torch datasets.
+        print("Creating Torch datasets")
+        self.__torch_train_dataset = GradientCrTorchDataset(pandas_dataframe=self.__pandas_train_dataset)
+        self.__torch_validation_dataset = GradientCrTorchDataset(pandas_dataframe=self.__pandas_validation_dataset)
+        self.__torch_test_dataset = GradientCrTorchDataset(pandas_dataframe=self.__pandas_test_dataset) if self.__pandas_test_dataset else None
+
+    def get_pil_image(self, item):
+        raise NotImplementedError("Not implemented")
+
+    def get_torch_image(self, item, processor):
+        raise NotImplementedError("Not implemented")
+
+    def get_labels(self):
+        return CR_CHEST_LABELS
+
+    def get_ids_to_labels(self):
+        raise NotImplementedError("Not implemented")
+
+    def get_labels_to_ids(self):
+        raise NotImplementedError("Not implemented")
+
+    def get_torch_label(self, item):
+        raise NotImplementedError("Not implemented")
+
+    def get_pandas_full_dataset(self):
+        raise NotImplementedError("Not implemented")
+
+    def get_hugging_face_train_dataset(self):
+        return datasets.Dataset.from_pandas(self.__pandas_train_dataset)
+
+    def get_hugging_face_validation_dataset(self):
+        return datasets.Dataset.from_pandas(self.__pandas_validation_dataset)
+
+    def get_hugging_face_test_dataset(self):
+        return datasets.Dataset.from_pandas(self.__pandas_test_dataset) if self.__pandas_test_dataset else None
+
+    def get_torch_train_dataset(self):
+         return self.__torch_train_dataset
+
+    def get_torch_validation_dataset(self):
+        return self.__torch_validation_dataset
+
+    def get_torch_test_dataset(self):
+        return self.__torch_test_dataset
+
+    def get_torch_train_data_loader(self, collate_function, batch_size, num_workers):
+        data_loader = DataLoader(self.__torch_train_dataset,
+                                 collate_fn=collate_function,
+                                 batch_size=batch_size,
+                                 shuffle=True,
+                                 num_workers=num_workers,
+                                 persistent_workers=True)
+        return data_loader
+
+    def get_torch_validation_data_loader(self, collate_function, batch_size, num_workers):
+        data_loader = DataLoader(self.__torch_validation_dataset,
+                                 collate_fn=collate_function,
+                                 batch_size=batch_size,
+                                 shuffle=False,
+                                 num_workers=num_workers,
+                                 persistent_workers=True)
+        return data_loader
+
+    def get_torch_test_data_loader(self, collate_function, batch_size, num_workers):
+        data_loader = DataLoader(self.__torch_test_dataset,
+                                 collate_fn=collate_function,
+                                 batch_size=batch_size,
+                                 shuffle=False,
+                                 num_workers=num_workers,
+                                 persistent_workers=True) if self.__torch_test_dataset else None
+        return data_loader
+
+
+class GradientCrTorchDataset(Dataset):
+    def __init__(self, pandas_dataframe):
+        self.__pandas_dataframe = pandas_dataframe
+
+    def __getitem__(self, idx):
+        item = self.__pandas_dataframe.iloc[idx]
+
+    def __len__(self):
+        return len(self.__pandas_dataframe)
