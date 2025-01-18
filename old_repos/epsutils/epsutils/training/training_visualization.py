4f3af077be76872326430a320a57a23e3f8ff78c
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-18 03:52:23 +0000

    Visualize single and 3 channel images.

--- old_repos/epsutils/epsutils/training/training_visualization.py
+++ old_repos/epsutils/epsutils/training/training_visualization.py
@@ -1,12 +1,22 @@
 import math
 
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
 from IPython.display import clear_output
 
 
 def show_visualization_data(visualization_data, num_view_grid_columns, label_to_string_mapping=None):
     inputs = visualization_data["inputs"]
-    labels = [label_to_string_mapping[label.item()] for label in visualization_data["labels"]] if label_to_string_mapping else "n/a"
+
+    if inputs.dtype == torch.bfloat16:
+        inputs = inputs.to(torch.float32)
+
+    if label_to_string_mapping:
+        labels = [label_to_string_mapping[label.item()] for label in visualization_data["labels"]]
+    else:
+        labels = ["-" for label in visualization_data["labels"]]
+
     probabilities = [probability.item() for probability in visualization_data["probabilities"]] if "probabilities" in visualization_data else None
 
     NUM_IMAGES = inputs.size(0)
@@ -17,7 +27,26 @@ def show_visualization_data(visualization_data, num_view_grid_columns, label_to_
 
     for i in range(NUM_IMAGES):
         plt.subplot(NUM_ROWS, num_view_grid_columns, i + 1)
-        plt.imshow(inputs[i, 0, :, :].cpu().numpy(), cmap="gray")
+
+        # Extract image from the batch.
+        input = inputs[i]
+
+        # Handle single and 3 channel images.
+        if input.shape[0] == 1:
+            input = input[0, :, :].cpu().numpy()
+        elif input.shape[0] == 3:
+            input = input.cpu().numpy().transpose(1, 2, 0)
+        else:
+            raise ValueError(f"Input image has unsupported number of channels {input.shape[0]}")
+
+        # Normalize image if necessary.
+        if input.dtype != np.uint8:
+            input_min = input.min(axis=(0, 1), keepdims=True)
+            input_max = input.max(axis=(0, 1), keepdims=True)
+            input = (input - input_min) / (input_max - input_min) * 255.0
+            input = input.astype(np.uint8)
+
+        plt.imshow(input, cmap="gray")
 
         title = f"Label: {labels[i]}"
 
