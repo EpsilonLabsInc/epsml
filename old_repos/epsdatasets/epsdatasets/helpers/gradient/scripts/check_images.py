01d238b84b86b09472de9507ddd5963106d09cdf
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-19 18:06:26 +0000

    Added script for detecting corrupt images.

---
+++ old_repos/epsdatasets/epsdatasets/helpers/gradient/scripts/check_images.py
@@ -0,0 +1,98 @@
+import ast
+import logging
+import os
+from concurrent.futures import ProcessPoolExecutor
+from enum import Enum
+from io import BytesIO
+
+from tqdm import tqdm
+
+from epsutils.dicom import dicom_utils
+from epsutils.gcs import gcs_utils
+from epsutils.logging import logging_utils
+
+
+IMAGES_FILE = "gs://epsilonlabs-filestore/cleaned_CRs/gradient_rm_bad_dcm_1211_nolabel.jsonl"
+DESTINATION_IMAGES_DIR = "/mnt/gradient/gradient-cxr/22JUL2024"
+OUTPUT_FILE = "./output/gradient_rm_bad_dcm_1211_nolabel-corrupt-images.csv"
+
+
+class FileType(Enum):
+    JSONL = 1
+    UNSUPPORTED = 2
+
+
+def get_file_type(file_name):
+    if file_name.endswith(".jsonl"):
+        return FileType.JSONL
+    else:
+        return FileType.UNSUPPORTED
+
+
+def load_file(file_name):
+    if gcs_utils.is_gcs_uri(file_name):
+        gcs_data = gcs_utils.split_gcs_uri(file_name)
+        content = gcs_utils.download_file_as_string(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+        content = BytesIO(content)
+    else:
+        raise ValueError(f"Load option not implemented")
+
+
+def parse_content(file_type, content):
+    if file_type == FileType.JSONL:
+        return parse_jsonl(content)
+    else:
+        raise ValueError("Parse option not implemented")
+
+
+def parse_jsonl(content):
+    images = []
+    rows = content.splitlines()
+
+    for row in rows:
+        row = ast.literal_eval(row)
+        image_list = row["image"]
+
+        for image in image_list:
+            if image.endswith(".txt"):
+                image = image.replace("_", "/").replace(".txt", ".dcm")
+            image_path = os.path.join(DESTINATION_IMAGES_DIR, image) if DESTINATION_IMAGES_DIR else image
+            images.append(image_path)
+
+    return images
+
+
+def check_image(image_path):
+    if gcs_utils.is_gcs_uri(image_path):
+        gcs_data = gcs_utils.split_gcs_uri(image_path)
+        content = gcs_utils.download_file_as_bytes(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+    else:
+        content = image_path
+
+    try:
+        image = dicom_utils.get_dicom_image(content, custom_windowing_parameters={"window_center": 0, "window_width": 0})
+        print(f"Ok image: {image_path}")
+    except Exception as e:
+        print(f"Corrupt image: {image_path}")
+        logging.warning(image_path)
+
+
+def main():
+    logging_utils.configure_logger(logger_file_name=OUTPUT_FILE, show_logging_category=False)
+
+    file_type = get_file_type(IMAGES_FILE)
+    if file_type == FileType.UNSUPPORTED:
+        raise ValueError("File type is unsupported")
+
+    print(f"Loading images file")
+    content = load_file(IMAGES_FILE)
+
+    print("Parsing content")
+    images = parse_content(file_type, content)
+
+    with ProcessPoolExecutor() as executor:  # Use default number of workers.
+        results = list(tqdm(executor.map(check_image, [image for image in images]), total=len(images), desc="Processing"))
+
+
+if __name__ == "__main__":
+    main()
