837f43d1160b0182f272bfa3c1c4cc9d97936bb9
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-21 09:32:02 +0000

    Raise exception on batch size 1.

--- old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/intern_vit_classifier.py
+++ old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/intern_vit_classifier.py
@@ -7,6 +7,8 @@ class InternVitClassifier(nn.Module):
     def __init__(self, num_classes, intern_vl_checkpoint_dir, intern_vit_output_dim=1024, hidden_dim=1024, dropout_rate=0.2):
         super().__init__()
 
+        print("WARNING: Because of BatchNorm1d that doesn't work on single element batches, InternVitClassifier currently supports only batch sizes >= 2")
+
         # InternViT model.
         self.intern_vit = InternVit(intern_vl_checkpoint_dir=intern_vl_checkpoint_dir)
 
@@ -28,6 +30,9 @@ class InternVitClassifier(nn.Module):
         self.classifier = self.classifier.to(dtype)
 
     def forward(self, x):
+        if x.shape[0] < 2:
+            raise ValueError("Because of BatchNorm1d that doesn't work on single element batches, InternVitClassifier currently supports only batch sizes >= 2")
+
         output = self.intern_vit(x)
         return self.classifier(output.pooler_output)
 
