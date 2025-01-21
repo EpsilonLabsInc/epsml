7b19a4cd073d4e62f2f86d6e6332fdc23bc603a6
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-21 21:11:55 +0000

    Implemented Dino ViT classifier.

---
+++ old_repos/epsclassifiers/epsclassifiers/dino_vit_classifier/dino_vit_classifier.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+
+from dino_vit import DinoVit, DinoVitType
+
+
+class DinoVitClassifier(nn.Module):
+    def __init__(self, num_classes, dino_vit_type: DinoVitType, dino_vit_checkpoint=None, dino_vit_output_dim=1024, hidden_dim=1024, dropout_rate=0.2):
+        super().__init__()
+
+        print("WARNING: Because of BatchNorm1d that doesn't work on single element batches, DinoVitClassifier currently supports only batch sizes >= 2")
+
+        # DinoViT model.
+        self.dino_vit = DinoVit(dino_vit_type=DinoVitType.LARGE, dino_vit_checkpoint=dino_vit_checkpoint)
+
+        # Classifier head.
+        self.classifier = nn.Sequential(
+            nn.Linear(dino_vit_output_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            nn.BatchNorm1d(hidden_dim // 2),
+            nn.GELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim // 2, num_classes)
+        )
+
+        # Set the dtype of the classifier to match the dtype of the DinoViT.
+        dtype = next(self.dino_vit.parameters()).dtype
+        self.classifier = self.classifier.to(dtype)
+
+    def forward(self, x):
+        if x.shape[0] < 2:
+            raise ValueError("Because of BatchNorm1d that doesn't work on single element batches, DinoVitClassifier currently supports only batch sizes >= 2")
+
+        output = self.dino_vit(x)
+        return self.classifier(output)
+
+    def get_image_processor(self):
+        return self.dino_vit.get_image_processor()
---
+++ old_repos/epsclassifiers/epsclassifiers/dino_vit_classifier/run_test.py
@@ -0,0 +1,34 @@
+import torch
+from PIL import Image
+
+from dino_vit import DinoVitType
+from epsutils.dicom import dicom_utils
+from epsclassifiers.dino_vit_classifier import DinoVitClassifier
+
+CHECKPOINT = "/home/andrej/work/2d-image-encoders/dino_vit/data/model_0479999.pth"
+IMAGE_PATH = "./samples/sample.dcm"
+
+
+def main():
+    classifier = DinoVitClassifier(num_classes=14, dino_vit_type=DinoVitType.LARGE, dino_vit_checkpoint=CHECKPOINT)
+    classifier = classifier.to("cuda")
+    image_processor = classifier.get_image_processor()
+
+    # Preprocess image.
+    image = dicom_utils.get_dicom_image(IMAGE_PATH, custom_windowing_parameters={"window_center": 0, "window_width": 0})
+    image = Image.fromarray(image).convert("RGB")
+    pixel_values = image_processor(images=[image, image], return_tensors="pt").pixel_values
+    pixel_values = pixel_values.cuda()
+
+    # Run inference.
+    output = classifier(pixel_values)
+
+    print(f"Input tensor size: {pixel_values.shape}")
+    print("Output:")
+    print(output)
+    print(f"Output type: {type(output)}")
+    print(f"Output size: {output.shape}")
+
+
+if __name__ == "__main__":
+    main()
