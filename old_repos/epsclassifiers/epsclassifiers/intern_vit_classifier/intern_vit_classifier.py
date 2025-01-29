0de70ca6dbe6892b97fce15175254de1f8345119
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-29 23:00:18 +0000

    Supported tiles.

--- old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/intern_vit_classifier.py
+++ old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/intern_vit_classifier.py
@@ -1,20 +1,24 @@
 import torch.nn as nn
 
+from epsutils.training.tile_splitting_image_processor import TileSplittingImageProcessor
 from intern_vit import InternVit
 
 
 class InternVitClassifier(nn.Module):
-    def __init__(self, num_classes, intern_vl_checkpoint_dir, intern_vit_output_dim=1024, hidden_dim=1024, dropout_rate=0.2):
+    def __init__(self, num_classes, intern_vl_checkpoint_dir, intern_vit_output_dim=1024, hidden_dim=1024, dropout_rate=0.2, use_tiles=False):
         super().__init__()
 
         print("WARNING: Because of BatchNorm1d that doesn't work on single element batches, InternVitClassifier currently supports only batch sizes >= 2")
+
+        self.__use_tiles = use_tiles
+        self.__intern_vit_output_dim = intern_vit_output_dim * 5 if use_tiles else intern_vit_output_dim
 
         # InternViT model.
         self.intern_vit = InternVit(intern_vl_checkpoint_dir=intern_vl_checkpoint_dir)
 
         # Classifier head.
         self.classifier = nn.Sequential(
-            nn.Linear(intern_vit_output_dim, hidden_dim),
+            nn.Linear(self.__intern_vit_output_dim, hidden_dim),
             nn.BatchNorm1d(hidden_dim),
             nn.GELU(),
             nn.Dropout(dropout_rate),
@@ -33,8 +37,20 @@ class InternVitClassifier(nn.Module):
         if x.shape[0] < 2:
             raise ValueError("Because of BatchNorm1d that doesn't work on single element batches, InternVitClassifier currently supports only batch sizes >= 2")
 
-        output = self.intern_vit(x)
-        return self.classifier(output.pooler_output)
+        if self.__use_tiles:
+            # 5 dimensions indicate use of tiles: (batch_num, num_tiles, num_channels, img_height, img_width)
+            assert len(x.shape) == 5
+            batch, tiles, num_channels, height, width = x.shape
+            x_reshaped = x.view(batch * tiles, num_channels, height, width)
+            output = self.intern_vit(x_reshaped)
+            reshaped_output = output.pooler_output.reshape(batch, -1)
+            return self.classifier(reshaped_output)
+        else:
+            output = self.intern_vit(x)
+            return self.classifier(output.pooler_output)
 
     def get_image_processor(self):
         return self.intern_vit.get_image_processor()
+
+    def get_tile_splitting_image_processor(self):
+        return TileSplittingImageProcessor(image_processor=self.intern_vit.get_image_processor())
--- old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/run_test.py
+++ old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/run_test.py
@@ -4,19 +4,28 @@ from PIL import Image
 from epsutils.dicom import dicom_utils
 from epsclassifiers.intern_vit_classifier import InternVitClassifier
 
-CHECKPOINT_DIR = "/mnt/training/internvl2.5_8b_finetune_lora_20241226_205132_1e-5_2.5_gradient_full_rm_sole_no_findings_rm_bad_dcm_tiles_6_no_labels/checkpoint-58670"
+CHECKPOINT_DIR = "/workspace/models/internvl2.5_26b_finetune_lora_20241229_184000_1e-5_2.5_gradient_full_rm_sole_no_findings_rm_bad_dcm_no_label/checkpoint-58670"
 IMAGE_PATH = "./samples/sample.dcm"
+USE_LARGER_MODEL = True
+USE_TILES = True
 
 
 def main():
-    classifier = InternVitClassifier(num_classes=14, intern_vl_checkpoint_dir=CHECKPOINT_DIR)
+    intern_vit_output_dim = 3200 if USE_LARGER_MODEL else 1024  # 3200 for InternVL 26B model and 1024 for InternVL 8B model.
+
+    classifier = InternVitClassifier(num_classes=14, intern_vl_checkpoint_dir=CHECKPOINT_DIR, intern_vit_output_dim=intern_vit_output_dim, use_tiles=USE_TILES)
     classifier = classifier.to("cuda")
-    image_processor = classifier.get_image_processor()
+    classifier.eval()
+    for param in classifier.parameters():
+        param.requires_grad = False
+
+    # Image processor.
+    image_processor = classifier.get_tile_splitting_image_processor() if USE_TILES else classifier.get_image_processor()
 
     # Preprocess image.
     image = dicom_utils.get_dicom_image(IMAGE_PATH, custom_windowing_parameters={"window_center": 0, "window_width": 0})
     image = Image.fromarray(image).convert("RGB")
-    pixel_values = image_processor(images=[image, image], return_tensors="pt").pixel_values
+    pixel_values = image_processor(images=[image, image], return_tensors="pt") if USE_TILES else image_processor(images=[image, image], return_tensors="pt").pixel_values
     pixel_values = pixel_values.to(torch.bfloat16).cuda()
 
     # Run inference.
