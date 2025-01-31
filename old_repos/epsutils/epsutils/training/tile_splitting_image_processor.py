b86d4d6ff3ae8fafee00017cfc56aaf44c805d69
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-31 13:20:19 +0000

    Configurable number of tiles.

--- old_repos/epsutils/epsutils/training/tile_splitting_image_processor.py
+++ old_repos/epsutils/epsutils/training/tile_splitting_image_processor.py
@@ -2,31 +2,39 @@ import torch
 
 
 class TileSplittingImageProcessor:
-    def __init__(self, image_processor):
+    def __init__(self, image_processor, num_rows=3, num_cols=3):
         self.__image_processor = image_processor
         assert self.__image_processor.crop_size["width"] == self.__image_processor.crop_size["height"]
         self.__output_image_size = self.__image_processor.crop_size["width"]
 
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+
+    def get_num_tiles(self):
+        return self.__num_rows * self.__num_cols + 1  # +1 for extra thumbnail.
+
     def __call__(self, images, return_tensors="pt"):
         return self.process_images(images, return_tensors)
 
-    def get_tiles(self, image):
+    def __get_tiles(self, image):
         width, height = image.size
-        mid_width, mid_height = width // 2, height // 2
+        tile_width, tile_height = width // self.__num_cols, height // self.__num_rows
+
         tiles = [
-            image,
-            image.crop((0, 0, mid_width, mid_height)),
-            image.crop((mid_width, 0, width, mid_height)),
-            image.crop((0, mid_height, mid_width, height)),
-            image.crop((mid_width, mid_height, width, height))
+            image.crop((i * tile_width, j * tile_height, (i + 1) * tile_width, (j + 1) * tile_height))
+            for j in range(self.__num_rows)
+            for i in range(self.__num_cols)
         ]
+
+        tiles.append(image)  # Add original image which will serve as the thumbnail.
+
         return tiles
 
     def process_images(self, images, return_tensors="pt"):
         processed_images = []
 
         for image in images:
-            tiles = self.get_tiles(image)
+            tiles = self.__get_tiles(image)
             processed_tiles = []
 
             for tile in tiles:
