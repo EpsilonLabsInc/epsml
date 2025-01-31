7cf9cefced70ad3e17ae1b1370d78ef59507c949
Andrej Ikica <andrej@epsilonlabs.ai>
2025-01-31 13:22:38 +0000

    Added labels with separated fractures.

--- old_repos/epsutils/epsutils/labels/cr_chest_labels.py
+++ old_repos/epsutils/epsutils/labels/cr_chest_labels.py
@@ -36,3 +36,24 @@ EXTENDED_CR_CHEST_LABELS = [
 ]
 
 assert len(EXTENDED_CR_CHEST_LABELS) == 15
+
+EXTENDED_CR_CHEST_LABELS_WITH_SEPARATED_FRACTURES = [
+    "Enlarged Cardiomediastinum",
+    "Cardiomegaly",
+    "Lung Lesion",
+    "Airspace Opacity",
+    "Edema",
+    "Consolidation",
+    "Pneumonia",
+    "Atelectasis",
+    "Pneumothorax",
+    "Pleural Effusion",
+    "Pleural Other",
+    "Fracture (Recent)",
+    "Fracture (Old/Healed)",
+    "Support Devices",
+    "No Findings",
+    "Other"
+]
+
+assert len(EXTENDED_CR_CHEST_LABELS_WITH_SEPARATED_FRACTURES) == 16
