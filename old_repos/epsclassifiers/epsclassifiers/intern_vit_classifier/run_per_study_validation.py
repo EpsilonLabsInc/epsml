3baedacd9c38e6c44801ca8f297b157997fd1d0d
Andrej Ikica <andrej@epsilonlabs.ai>
2025-02-04 20:32:50 +0000

    Support both GCS and local image paths.

--- old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/run_per_study_validation.py
+++ old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/run_per_study_validation.py
@@ -1,9 +1,11 @@
 import ast
+import os
 from io import BytesIO
 
 import numpy as np
 import torch
 from PIL import Image
+from tqdm import tqdm
 
 from epsutils.dicom import dicom_utils
 from epsutils.gcs import gcs_utils
@@ -13,6 +15,8 @@ from epsclassifiers.intern_vit_classifier import InternVitClassifier
 INITIAL_CHECKPOINT_DIR = "/workspace/models/old/internvl2.5_26b_finetune_lora_20241229_184000_1e-5_2.5_gradient_full_rm_sole_no_findings_rm_bad_dcm_no_label/checkpoint-58670"
 TRAINING_CHECKPOINT = "/workspace/models/intern_vit_classifier-training-on-gradient_cr_pneumothorax/intern_vit_pneumothorax_checkpoint_epoch_4_20250129_203157.pt"
 GCS_VALIDATION_FILE = "gs://gradient-crs/archive/training/gradient-crs-09JAN2025-per-study-chest-images-with-pneumothorax-label-validation.jsonl"
+SOURCE_IMAGE_PATH = "gs://epsilon-data-us-central1/GRADIENT-DATABASE/CR/09JAN2025/deid"
+TARGET_IMAGE_PATH = "/workspace/CR/09JAN2025"
 VALIDATE_POSITIVE_ONLY = False
 MAX_VALIDATION_COUNT = None
 
@@ -34,7 +38,7 @@ def main():
     y_pred = []
     lines = content.splitlines()
 
-    for line in lines:
+    for line in tqdm(lines, total=len(lines), desc="Processing"):
         line = ast.literal_eval(line)
 
         image_paths = line["image_path"]
@@ -49,10 +53,16 @@ def main():
         res = []
 
         for image_path in image_paths:
-            gcs_data = gcs_utils.split_gcs_uri(image_path)
-            data = gcs_utils.download_file_as_bytes(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+            if SOURCE_IMAGE_PATH and TARGET_IMAGE_PATH and image_path.startswith(SOURCE_IMAGE_PATH):
+                image_path = os.path.join(TARGET_IMAGE_PATH, os.path.relpath(image_path, SOURCE_IMAGE_PATH))
 
-            image = dicom_utils.get_dicom_image(BytesIO(data), custom_windowing_parameters={"window_center": 0, "window_width": 0})
+            if gcs_utils.is_gcs_uri(image_path):
+                gcs_data = gcs_utils.split_gcs_uri(image_path)
+                data = BytesIO(gcs_utils.download_file_as_bytes(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"]))
+            else:
+                data = image_path
+
+            image = dicom_utils.get_dicom_image(data, custom_windowing_parameters={"window_center": 0, "window_width": 0})
             image = image.astype(np.float32)
             eps = 1e-10
             image = (image - image.min()) / (image.max() - image.min() + eps) * 255
