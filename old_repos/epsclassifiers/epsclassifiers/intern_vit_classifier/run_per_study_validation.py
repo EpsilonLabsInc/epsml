6e1af3b4ca7021f8ec4a54ee88e59dc9f7ce9d6e
Andrej Ikica <andrej@epsilonlabs.ai>
2025-02-04 01:15:07 +0000

    Added per-study validation script.

---
+++ old_repos/epsclassifiers/epsclassifiers/intern_vit_classifier/run_per_study_validation.py
@@ -0,0 +1,86 @@
+import ast
+from io import BytesIO
+
+import numpy as np
+import torch
+from PIL import Image
+
+from epsutils.dicom import dicom_utils
+from epsutils.gcs import gcs_utils
+from epsutils.training.confusion_matrix_calculator import ConfusionMatrixCalculator
+from epsclassifiers.intern_vit_classifier import InternVitClassifier
+
+INITIAL_CHECKPOINT_DIR = "/workspace/models/old/internvl2.5_26b_finetune_lora_20241229_184000_1e-5_2.5_gradient_full_rm_sole_no_findings_rm_bad_dcm_no_label/checkpoint-58670"
+TRAINING_CHECKPOINT = "/workspace/models/intern_vit_classifier-training-on-gradient_cr_pneumothorax/intern_vit_pneumothorax_checkpoint_epoch_4_20250129_203157.pt"
+GCS_VALIDATION_FILE = "gs://gradient-crs/archive/training/gradient-crs-09JAN2025-per-study-chest-images-with-pneumothorax-label-validation.jsonl"
+VALIDATE_POSITIVE_ONLY = False
+MAX_VALIDATION_COUNT = None
+
+
+def main():
+    print("Creating model")
+    classifier = InternVitClassifier(num_classes=1, intern_vl_checkpoint_dir=INITIAL_CHECKPOINT_DIR, intern_vit_output_dim=3200)  # 3200 for InternVL 26B model, 1024 for InternVL 8B model.
+    training_checkpoint = torch.load(TRAINING_CHECKPOINT)
+    classifier.load_state_dict(training_checkpoint["model_state_dict"])
+    classifier = classifier.to("cuda")
+    classifier.eval()
+    image_processor = classifier.get_image_processor()
+
+    print(f"Downloading validation file {GCS_VALIDATION_FILE}")
+    gcs_data = gcs_utils.split_gcs_uri(GCS_VALIDATION_FILE)
+    content = gcs_utils.download_file_as_string(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+
+    y_true = []
+    y_pred = []
+    lines = content.splitlines()
+
+    for line in lines:
+        line = ast.literal_eval(line)
+
+        image_paths = line["image_path"]
+        labels = line["labels"]
+
+        if VALIDATE_POSITIVE_ONLY and not labels:
+            continue
+
+        if MAX_VALIDATION_COUNT and len(y_pred) >= MAX_VALIDATION_COUNT:
+            break
+
+        res = []
+
+        for image_path in image_paths:
+            gcs_data = gcs_utils.split_gcs_uri(image_path)
+            data = gcs_utils.download_file_as_bytes(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+
+            image = dicom_utils.get_dicom_image(BytesIO(data), custom_windowing_parameters={"window_center": 0, "window_width": 0})
+            image = image.astype(np.float32)
+            eps = 1e-10
+            image = (image - image.min()) / (image.max() - image.min() + eps) * 255
+            image = image.astype(np.uint8)
+            image = Image.fromarray(image)
+            image = image.convert("RGB")
+
+            pixel_values = image_processor(images=[image, image], return_tensors="pt").pixel_values
+            pixel_values = pixel_values.to(torch.bfloat16).cuda()
+
+            output = classifier(pixel_values)[0]
+            probabilities = torch.sigmoid(output)
+            assert probabilities.shape == (1,)
+            res.append(probabilities[0] >= 0.5)
+
+            del pixel_values
+            del output
+            del probabilities
+            torch.cuda.empty_cache()
+
+        y_true.append(1 if len(labels) > 0 else 0)
+        y_pred.append(1 if any(res) else 0)
+
+    calc = ConfusionMatrixCalculator()
+    cm = calc.compute_confusion_matrix(y_true=y_true, y_pred=y_pred)
+    print("Confusion matrix:")
+    print(cm)
+
+
+if __name__ == "__main__":
+    main()
