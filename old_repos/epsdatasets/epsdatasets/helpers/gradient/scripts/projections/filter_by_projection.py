ca8b416649040349a9cdda691c2c2369fee3d3ed
Andrej Ikica <andrej@epsilonlabs.ai>
2025-02-14 22:19:52 +0000

    Added frontal/lateral projection.

--- old_repos/epsdatasets/epsdatasets/helpers/gradient/scripts/projections/filter_by_projection.py
+++ old_repos/epsdatasets/epsdatasets/helpers/gradient/scripts/projections/filter_by_projection.py
@@ -1,21 +1,31 @@
 import logging
 import os
 from concurrent.futures import ProcessPoolExecutor
+from enum import Enum
 
 from tqdm import tqdm
 
 from epsutils.gcs import gcs_utils
 from epsutils.logging import logging_utils
 
+class Projection(Enum):
+    FRONTAL = 1
+    LATERAL = 2
+
 EPSILON_GCS_BUCKET_NAME = "gradient-crs"
 EPSILON_GCS_IMAGES_DIR = "22JUL2024"
 GRADIENT_GCS_IMAGES_DIR = "GRADIENT-DATABASE/CR/22JUL2024"
 CONTENT_TO_SEARCH = "(0008,103E) Series Description:"
+PROJECTION = Projection.FRONTAL
 OUTPUT_FILE = "gradient-crs-22JUL2024-frontal-views.csv"
 
 
 def process_file(file):
-    dicom_content = gcs_utils.download_file_as_string(gcs_bucket_name=EPSILON_GCS_BUCKET_NAME, gcs_file_name=file)
+    try:
+        dicom_content = gcs_utils.download_file_as_string(gcs_bucket_name=EPSILON_GCS_BUCKET_NAME, gcs_file_name=file)
+    except Exception as e:
+        print(f"Error processing {file}: {e}")
+        return
 
     if CONTENT_TO_SEARCH not in dicom_content:
         return
@@ -26,9 +36,16 @@ def process_file(file):
             series_description = line[len(CONTENT_TO_SEARCH):].strip()
             words = series_description.lower().split()
 
-            if {"pa", "ap"}.intersection(words) and {"chest", "rib", "ribs"}.intersection(words) and not {"lateral", "lat"}.intersection(words):
-                gradient_file_name = os.path.join(GRADIENT_GCS_IMAGES_DIR, os.path.basename(file).replace("_", "/").replace(".txt", ".dcm"))
-                logging.info(f"{gradient_file_name};{series_description}")
+            if PROJECTION == Projection.FRONTAL:
+                if {"pa", "ap"}.intersection(words) and {"chest", "rib", "ribs"}.intersection(words) and not {"lateral", "lat"}.intersection(words):
+                    gradient_file_name = os.path.join(GRADIENT_GCS_IMAGES_DIR, os.path.basename(file).replace("_", "/").replace(".txt", ".dcm"))
+                    logging.info(f"{gradient_file_name};{series_description}")
+            elif PROJECTION == Projection.LATERAL:
+                if {"lat", "lateral"}.intersection(words) and {"chest", "rib", "ribs"}.intersection(words) and not {"pa", "ap"}.intersection(words):
+                    gradient_file_name = os.path.join(GRADIENT_GCS_IMAGES_DIR, os.path.basename(file).replace("_", "/").replace(".txt", ".dcm"))
+                    logging.info(f"{gradient_file_name};{series_description}")
+            else:
+                raise ValueError(f"Unsupported projection {PROJECTION}")
 
             return
 
