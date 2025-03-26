08467e5e82fef6d1bba092f592b39df3ac813bf4
Andrej Ikica <andrej@epsilonlabs.ai>
2025-03-26 22:17:38 +0000

    Supported batch mode.

--- old_repos/epsclassifiers/epsclassifiers/med_image_parse_segmentor/med_image_parse_segmentor.py
+++ old_repos/epsclassifiers/epsclassifiers/med_image_parse_segmentor/med_image_parse_segmentor.py
@@ -16,20 +16,46 @@ class MedImageParseSegmentor:
         self.__endpoint_url = endpoint_url
         self.__auth_key = auth_key
         self.__transfer_image_size = transfer_image_size
-        self.__input_image = None
+        self.__input_images = None
+        self.__preprocessed_images = None
+        self.__encoded_images = None
 
-    def segment(self, image, prompt):
-        return self.__segment_impl(image, prompt)
+    def segment(self, images, prompt):
+        return self.__segment_impl(images, prompt)
 
-    def __segment_impl(self, image, prompt):
-        if isinstance(image, str):
-            data = self.__dicom_to_bytes_stream(image) if image.endswith(".dcm") else open(image, "rb").read()
-        else:
-            data = image
+    def __segment_impl(self, images, prompt):
+        assert len(images) > 0
 
-        self.__input_image = Image.open(BytesIO(data))
-        encoded_image = self.__to_base64(data)
-        request_data = self.__generate_request_data(encoded_image, prompt)
+        self.__input_images = []
+        self.__preprocessed_images = []
+        self.__encoded_images = []
+
+        # Create a list of input images.
+        for image in images:
+            if isinstance(image, str) and image.endswith(".dcm"):
+                self.__input_images.append(self.__dicom_to_image(image))
+            elif isinstance(image, str):
+                self.__input_images.append(Image.open(image))
+            elif isinstance(image, Image.Image):
+                self.__input_images.append(image)
+            else:
+                raise ValueError("Unsupported input image")
+
+        # Preprocess images.
+        for image in self.__input_images:
+            preprocessed_image = image.resize(self.__transfer_image_size) if self.__transfer_image_size else image
+            self.__preprocessed_images.append(preprocessed_image)
+
+        # Encode images.
+        for image in self.__preprocessed_images:
+            bytes_stream = BytesIO()
+            image.save(bytes_stream, format="PNG")
+            data = bytes_stream.getvalue()
+            bytes_stream.close()
+            encoded_image = self.__to_base64(data)
+            self.__encoded_images.append(encoded_image)
+
+        request_data = self.__generate_request_data(self.__encoded_images, prompt)
         response = self.__send_request(request_data)
         return response
 
@@ -39,26 +65,17 @@ class MedImageParseSegmentor:
     def __from_base64(self, data):
         return base64.b64decode(data)
 
-    def __dicom_to_bytes_stream(self, dicom_file):
+    def __dicom_to_image(self, dicom_file):
         image = dicom_utils.get_dicom_image(dicom_file, custom_windowing_parameters={"window_center": 0, "window_width": 0})
-
-        if self.__transfer_image_size:
-            image = cv2.resize(image, self.__transfer_image_size, interpolation=cv2.INTER_LINEAR)
-
         image = image_utils.numpy_array_to_pil_image(image, convert_to_uint8=True, convert_to_rgb=False)
-        bytes_stream = BytesIO()
-        image.save(bytes_stream, format="PNG")
-        data = bytes_stream.getvalue()
-        bytes_stream.close()
+        return image
 
-        return data
-
-    def __generate_request_data(self, encoded_image, text):
+    def __generate_request_data(self, encoded_images, text):
         data = {
             "input_data": {
                 "columns": ["image", "text"],
-                "index": [0],
-                "data": [[encoded_image, text]]
+                "index": list(range(len(encoded_images))),
+                "data": [[encoded_image, text] for encoded_image in encoded_images]
             }
         }
 
@@ -69,33 +86,48 @@ class MedImageParseSegmentor:
         response = requests.post(self.__endpoint_url, headers=headers, data=data)
 
         if response.status_code != 200:
-            return {"input_image": self.__input_image, "segmentation_image": None, "error_code": response.status_code, "error_text": response.text}
+            return {
+                "input_images": self.__input_images,
+                "preprocessed_images": self.__preprocessed_images,
+                "segmentation_images": None,
+                "error_code": response.status_code,
+                "error_text": response.text
+            }
 
         content = response.json()
-        image_features = json.loads(content[0]["image_features"])
-        image_data = image_features["data"]
-        image_shape = tuple(image_features["shape"][1:])  # Skip batch dimension.
-        image_dtype = image_features["dtype"]
+        segmentation_images = []
 
-        image_bytes = self.__from_base64(image_data)
-        image_array = np.frombuffer(image_bytes, dtype=image_dtype)
-        image_array = image_array.reshape(image_shape)
-        segmentation_image = Image.fromarray(image_array)
+        for item in content:
+            image_features = json.loads(item["image_features"])
+            image_data = image_features["data"]
+            image_shape = tuple(image_features["shape"][1:])  # Skip batch dimension.
+            image_dtype = image_features["dtype"]
 
-        return {"input_image": self.__input_image, "segmentation_image": segmentation_image, "error_code": None, "error_message": None}
+            image_bytes = self.__from_base64(image_data)
+            image_array = np.frombuffer(image_bytes, dtype=image_dtype)
+            image_array = image_array.reshape(image_shape)
+            segmentation_image = Image.fromarray(image_array)
+            segmentation_images.append(segmentation_image)
+
+        return {
+            "input_images": self.__input_images,
+            "preprocessed_images": self.__preprocessed_images,
+            "segmentation_images": segmentation_images,
+            "error_code": None,
+            "error_message": None
+        }
 
 
 if __name__ == "__main__":
     print("Running segmentation example")
 
-    # image_path = "./samples/covid_1585.png"
-    image_path = "./samples/1.dcm"
+    image_paths = ["./samples/covid_1585.png", "./samples/1.dcm"]
     prompt = "segment chest"
     endpoint_url = "https://epsilon-ml-eastus-medimageparse.eastus2.inference.ml.azure.com/score"
     auth_key = "B2CAKaGiUPuTEQ5oAUJq6sPO8uqDlChgONuZgm7XZGMW2o1ycTmwJQQJ99BCAAAAAAAAAAAAINFRAZML4PbT"
 
     segmentor = MedImageParseSegmentor(endpoint_url=endpoint_url, auth_key=auth_key)
-    result = segmentor.segment(image=image_path, prompt=prompt)
+    result = segmentor.segment(images=image_paths, prompt=prompt)
 
     if result["error_code"]:
         print("Received error response")
@@ -103,10 +135,10 @@ if __name__ == "__main__":
         print(f"Error message: {result['error_message']}")
         exit()
 
-    input_image = result["input_image"]
-    segmentation_image = result["segmentation_image"]
+    for index, preprocessed_image in enumerate(result["preprocessed_images"]):
+        preprocessed_image.save(f"preprocessed_image_{index}.png")
 
-    input_image.save("input_image.png")
-    segmentation_image.save("segmentation_image.png")
+    for index, segmentation_image in enumerate(result["segmentation_images"]):
+        segmentation_image.save(f"segmentation_image_{index}.png")
 
     print("Input and segmentation images saved")
