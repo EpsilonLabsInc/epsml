4c3913570d7885c0ea7c1c89d3817682c0f12967
Andrej Ikica <andrej@epsilonlabs.ai>
2025-03-27 09:25:22 +0000

    Added cropping functions.

--- old_repos/epsutils/epsutils/image/image_utils.py
+++ old_repos/epsutils/epsutils/image/image_utils.py
@@ -71,3 +71,49 @@ def numpy_array_to_pil_image(image_array, convert_to_uint8=True, convert_to_rgb=
         image = image.convert("RGB")
 
     return image
+
+
+def min_bounding_rectangle(image_mask, padding_ratio=0.0, return_relative_coordinates=True):
+    image_mask = np.asarray(image_mask)
+
+    if not np.any(image_mask):
+        return None
+
+    coords = np.argwhere(image_mask)
+
+    y_min, x_min = coords.min(axis=0)
+    y_max, x_max = coords.max(axis=0)
+
+    width = x_max - x_min + 1
+    height = y_max - y_min + 1
+
+    padding_x = int(width * padding_ratio / 2)
+    padding_y = int(height * padding_ratio / 2)
+
+    x_min = max(0, x_min - padding_x)
+    y_min = max(0, y_min - padding_y)
+    x_max = min(image_mask.shape[1] - 1, x_max + padding_x)
+    y_max = min(image_mask.shape[0] - 1, y_max + padding_y)
+
+    if return_relative_coordinates:
+        x_min_rel = x_min / image_mask.shape[1]
+        y_min_rel = y_min / image_mask.shape[0]
+        x_max_rel = x_max / image_mask.shape[1]
+        y_max_rel = y_max / image_mask.shape[0]
+
+        return x_min_rel, y_min_rel, x_max_rel, y_max_rel
+    else:
+        return x_min, y_min, x_max, y_max
+
+
+def crop_image(image, crop_coordinates, use_relative_coordinates=False):
+    if use_relative_coordinates:
+        width, height = image.size
+        x_min = int(crop_coordinates[0] * width)
+        y_min = int(crop_coordinates[1] * height)
+        x_max = int(crop_coordinates[2] * width)
+        y_max = int(crop_coordinates[3] * height)
+
+        return image.crop((x_min, y_min, x_max, y_max))
+    else:
+        return image.crop(crop_coordinates)
