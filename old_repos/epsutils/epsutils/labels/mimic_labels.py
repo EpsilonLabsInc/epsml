1929d177cff4a21e39b3d7f94d1bd6e196b8327e
Andrej Ikica <andrej@epsilonlabs.ai>
2025-04-25 14:20:42 +0000

    Added list of Mimic labels.

---
+++ old_repos/epsutils/epsutils/labels/mimic_labels.py
@@ -0,0 +1,16 @@
+MIMIC_LABELS = [
+    "Atelectasis",
+    "Cardiomegaly",
+    "Consolidation",
+    "Edema",
+    "Enlarged Cardiomediastinum",
+    "Fracture",
+    "Lung Lesion",
+    "Lung Opacity",
+    "No Finding",
+    "Pleural Effusion",
+    "Pleural Other",
+    "Pneumonia",
+    "Pneumothorax",
+    "Support Devices"
+]
