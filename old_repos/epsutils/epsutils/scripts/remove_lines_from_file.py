d168200c83f92c4299d7aa39462737633412ccba
Andrej Ikica <andrej@epsilonlabs.ai>
2025-06-13 12:36:35 +0000

    Supported multiple modes.

--- old_repos/epsutils/epsutils/scripts/remove_lines_from_file.py
+++ old_repos/epsutils/epsutils/scripts/remove_lines_from_file.py
@@ -1,23 +1,48 @@
 import argparse
+from enum import Enum
+
+
+class Mode(Enum):
+    REMOVE_IF_INCLUDES = 1
+    REMOVE_IF_NOT_INCLUDES = 2
+    REMOVE_IF_STARTS_WITH = 3
+    REMOVE_IF_NOT_STARTS_WITH = 4
 
 
 def main(args):
     with open(args.input_file_path, "r") as file:
         lines = file.readlines()
 
-    filtered_lines = [line for line in lines if args.line_content_to_remove not in line]
+    filtered_lines = []
+    for line in lines:
+        if args.mode == Mode.REMOVE_IF_INCLUDES:
+            if args.content not in line:
+                filtered_lines.append(line)
+        elif args.mode == Mode.REMOVE_IF_NOT_INCLUDES:
+            if args.content in line:
+                filtered_lines.append(line)
+        elif args.mode == Mode.REMOVE_IF_STARTS_WITH:
+            if not line.startswith(args.content):
+                filtered_lines.append(line)
+        elif args.mode == Mode.REMOVE_IF_NOT_STARTS_WITH:
+            if line.startswith(args.content):
+                filtered_lines.append(line)
+        else:
+            raise ValueError(f"Unsupported mode {args.mode}")
 
     with open(args.output_file_path, "w") as file:
         file.writelines(filtered_lines)
 
 
 if __name__ == "__main__":
-    INPUT_FILE_PATH = "/mnt/efs/all-cxr/simonmed/batch1/simonmed_batch_1_reports_with_image_paths_filtered.csv_20250527_121901.log"
-    LINE_CONTENT_TO_REMOVE = "WARNING: decode_data_sequence is deprecated and will be removed in v4.0"
-    OUTPUT_FILE_PATH = "/mnt/efs/all-cxr/simonmed/batch1/simonmed_batch_1_reports_with_image_paths_filtered.csv_20250527_121901_new.log"
+    INPUT_FILE_PATH = "/home/andrej/tmp/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_projections.csv"
+    CONTENT = "/mnt/efs"
+    MODE = Mode.REMOVE_IF_NOT_STARTS_WITH
+    OUTPUT_FILE_PATH = "/home/andrej/tmp/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_projections.csv"
 
     args = argparse.Namespace(input_file_path=INPUT_FILE_PATH,
-                              line_content_to_remove=LINE_CONTENT_TO_REMOVE,
+                              content=CONTENT,
+                              mode=MODE,
                               output_file_path=OUTPUT_FILE_PATH)
 
     main(args)
