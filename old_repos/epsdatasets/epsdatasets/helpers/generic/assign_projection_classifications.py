e49f8fd24d668f2f1c10471b3d0cbbd7a3157258
Andrej Ikica <andrej@epsilonlabs.ai>
2025-06-13 13:25:46 +0000

    Refactoring.

--- old_repos/epsdatasets/epsdatasets/helpers/generic/assign_projection_classifications.py
+++ old_repos/epsdatasets/epsdatasets/helpers/generic/assign_projection_classifications.py
@@ -6,7 +6,7 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def assign_projections(args):
+def assign_projection_classifications(args):
     # Load input CSV file.
     print(f"Loading input CSV file {args.input_csv_path}")
     df = pd.read_csv(args.input_csv_path, low_memory=False)
@@ -23,28 +23,28 @@ def assign_projections(args):
         else:
             image_paths[i] = None
 
-    # Load projections CSV file.
-    print(f"Loading projections CSV file {args.projections_csv_path}")
-    projections_df = pd.read_csv(args.projections_csv_path, low_memory=False, header=None)
+    # Load projection classifications CSV file.
+    print(f"Loading projection classifications CSV file {args.projection_classifications_csv_path}")
+    projection_classifications_df = pd.read_csv(args.projection_classifications_csv_path, low_memory=False, header=None)
 
-    # Create projections dict.
-    print("Creating projections dict")
-    projections_dict = dict(zip(projections_df[0], projections_df[1]))
+    # Create projection classifications dict.
+    print("Creating projection classifications dict")
+    projection_classifications_dict = dict(zip(projection_classifications_df[0], projection_classifications_df[1]))
 
-    # Assign projections.
-    projections = []
-    print("Assigning projections")
+    # Assign projection classifications.
+    projection_classifications = []
+    print("Assigning projection classifications")
     for image_path in image_paths:
         if image_path is None:
-            projections.append(None)
+            projection_classifications.append(None)
         else:
-            values = [projections_dict[item] if item in projections_dict else None for item in image_path]
-            projections.append(values)
+            values = [projection_classifications_dict[item] if item in projection_classifications_dict else None for item in image_path]
+            projection_classifications.append(values)
 
     # Update projection classification column.
     print("Updating projection classification column")
-    assert len(projections) == len(df)
-    df["projection_classification"] = projections
+    assert len(projection_classifications) == len(df)
+    df["projection_classification"] = projection_classifications
 
     # Save output.
     print(f"Saving output file {args.output_file_path}")
@@ -52,12 +52,12 @@ def assign_projections(args):
 
 
 def main(args):
-    assign_projections(args)
+    assign_projection_classifications(args)
 
 
 if __name__ == "__main__":
     INPUT_CSV_PATH = "/mnt/efs/all-cxr/combined/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all.csv"
-    PROJECTIONS_CSV_PATH = "/home/andrej/tmp/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_projections.csv"
+    PROJECTION_CLASSIFICATIONS_CSV_PATH = "/home/andrej/tmp/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_projections.csv"
     OUTPUT_FILE_PATH = "/mnt/efs/all-cxr/combined/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_with_projections.csv"
     BASE_PATH_SUBSTITUTIONS = {
         "gradient/22JUL2024": "/mnt/efs/all-cxr/gradient/22JUL2024",
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     }
 
     args = argparse.Namespace(input_csv_path=INPUT_CSV_PATH,
-                              projections_csv_path=PROJECTIONS_CSV_PATH,
+                              projection_classifications_csv_path=PROJECTION_CLASSIFICATIONS_CSV_PATH,
                               output_file_path=OUTPUT_FILE_PATH,
                               base_path_substitutions=BASE_PATH_SUBSTITUTIONS)
 
