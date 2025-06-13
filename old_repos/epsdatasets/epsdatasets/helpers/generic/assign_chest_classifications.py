9061dbd881a8cd279b8f6ecdf1c5bdd2009eebc7
Andrej Ikica <andrej@epsilonlabs.ai>
2025-06-13 13:31:51 +0000

    Implemented script for assigning chest classifications.

--- old_repos/epsdatasets/epsdatasets/helpers/generic/assign_chest_classifications.py
+++ old_repos/epsdatasets/epsdatasets/helpers/generic/assign_chest_classifications.py
@@ -6,7 +6,7 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def assign_projection_classifications(args):
+def assign_chest_classifications(args):
     # Load input CSV file.
     print(f"Loading input CSV file {args.input_csv_path}")
     df = pd.read_csv(args.input_csv_path, low_memory=False)
@@ -23,28 +23,28 @@ def assign_projection_classifications(args):
         else:
             image_paths[i] = None
 
-    # Load projection classifications CSV file.
-    print(f"Loading projection classifications CSV file {args.projection_classifications_csv_path}")
-    projection_classifications_df = pd.read_csv(args.projection_classifications_csv_path, low_memory=False, header=None)
+    # Load chest classifications CSV file.
+    print(f"Loading chest classifications CSV file {args.chest_classifications_csv_path}")
+    chest_classifications_df = pd.read_csv(args.chest_classifications_csv_path, low_memory=False, header=None)
 
-    # Create projection classifications dict.
-    print("Creating projection classifications dict")
-    projection_classifications_dict = dict(zip(projection_classifications_df[0], projection_classifications_df[1]))
+    # Create chest classifications dict.
+    print("Creating chest classifications dict")
+    chest_classifications_dict = dict(zip(chest_classifications_df[0], chest_classifications_df[1]))
 
-    # Assign projection classifications.
-    projection_classifications = []
-    print("Assigning projection classifications")
+    # Assign chest classifications.
+    chest_classifications = []
+    print("Assigning chest classifications")
     for image_path in image_paths:
         if image_path is None:
-            projection_classifications.append(None)
+            chest_classifications.append(None)
         else:
-            values = [projection_classifications_dict[item] if item in projection_classifications_dict else None for item in image_path]
-            projection_classifications.append(values)
+            values = [chest_classifications_dict[item] if item in chest_classifications_dict else None for item in image_path]
+            chest_classifications.append(values)
 
-    # Update projection classification column.
-    print("Updating projection classification column")
-    assert len(projection_classifications) == len(df)
-    df["projection_classification"] = projection_classifications
+    # Update chest classification column.
+    print("Updating chest classification column")
+    assert len(chest_classifications) == len(df)
+    df["chest_classification"] = chest_classifications
 
     # Save output.
     print(f"Saving output file {args.output_file_path}")
@@ -52,13 +52,13 @@ def assign_projection_classifications(args):
 
 
 def main(args):
-    assign_projection_classifications(args)
+    assign_chest_classifications(args)
 
 
 if __name__ == "__main__":
     INPUT_CSV_PATH = "/mnt/efs/all-cxr/combined/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all.csv"
-    PROJECTION_CLASSIFICATIONS_CSV_PATH = "/home/andrej/tmp/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_projection_classifications.csv"
-    OUTPUT_FILE_PATH = "/mnt/efs/all-cxr/combined/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_with_projection_classifications.csv"
+    CHEST_CLASSIFICATIONS_CSV_PATH = "/home/andrej/tmp/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_chest_classifications.csv"
+    OUTPUT_FILE_PATH = "/mnt/efs/all-cxr/combined/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_with_chest_classifications.csv"
     BASE_PATH_SUBSTITUTIONS = {
         "gradient/22JUL2024": "/mnt/efs/all-cxr/gradient/22JUL2024",
         "gradient/20DEC2024": "/mnt/efs/all-cxr/gradient/20DEC2024/deid",
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     }
 
     args = argparse.Namespace(input_csv_path=INPUT_CSV_PATH,
-                              projection_classifications_csv_path=PROJECTION_CLASSIFICATIONS_CSV_PATH,
+                              chest_classifications_csv_path=CHEST_CLASSIFICATIONS_CSV_PATH,
                               output_file_path=OUTPUT_FILE_PATH,
                               base_path_substitutions=BASE_PATH_SUBSTITUTIONS)
 
