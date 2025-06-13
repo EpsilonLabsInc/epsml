24ef767aa90423d294c44418798750b25ea9e90f
Andrej Ikica <andrej@epsilonlabs.ai>
2025-06-13 12:57:34 +0000

    Implemented script for assigning projections.

---
+++ old_repos/epsdatasets/epsdatasets/helpers/generic/assign_projections.py
@@ -0,0 +1,80 @@
+import argparse
+import ast
+import os
+
+import pandas as pd
+from tqdm import tqdm
+
+
+def assign_projections(args):
+    # Load input CSV file.
+    print(f"Loading input CSV file {args.input_csv_path}")
+    df = pd.read_csv(args.input_csv_path, low_memory=False)
+
+    # Update image paths.
+    print("Updating image paths")
+    image_paths = df["image_paths"].tolist()
+    base_paths = df["base_path"].tolist()
+    for i, (image_path, base_path) in tqdm(enumerate(zip(image_paths, base_paths)), total=len(image_paths), desc="Processing"):
+        image_path = ast.literal_eval(image_path)
+        subst = args.base_path_substitutions[base_path]
+        if subst is not None:
+            image_paths[i] = [os.path.join(subst, item) for item in image_path]
+        else:
+            image_paths[i] = None
+
+    # Load projections CSV file.
+    print(f"Loading projections CSV file {args.projections_csv_path}")
+    projections_df = pd.read_csv(args.projections_csv_path, low_memory=False, header=None)
+
+    # Create projections dict.
+    print("Creating projections dict")
+    projections_dict = dict(zip(projections_df[0], projections_df[1]))
+
+    # Assign projections.
+    projections = []
+    print("Assigning projections")
+    for image_path in image_paths:
+        if image_path is None:
+            projections.append(None)
+        else:
+            values = [projections_dict[item] if item in projections_dict else None for item in image_path]
+            projections.append(values)
+
+    # Update projection classification column.
+    print("Updating projection classification column")
+    assert len(projections) == len(df)
+    df["projection_classification"] = projections
+
+    # Save output.
+    print(f"Saving output file {args.output_file_path}")
+    df.to_csv(args.output_file_path, index=False)
+
+
+def main(args):
+    assign_projections(args)
+
+
+if __name__ == "__main__":
+    INPUT_CSV_PATH = "/mnt/efs/all-cxr/combined/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all.csv"
+    PROJECTIONS_CSV_PATH = "/home/andrej/tmp/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_projections.csv"
+    OUTPUT_FILE_PATH = "/mnt/efs/all-cxr/combined/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all_with_projections.csv"
+    BASE_PATH_SUBSTITUTIONS = {
+        "gradient/22JUL2024": "/mnt/efs/all-cxr/gradient/22JUL2024",
+        "gradient/20DEC2024": "/mnt/efs/all-cxr/gradient/20DEC2024/deid",
+        "gradient/09JAN2025": "/mnt/efs/all-cxr/gradient/09JAN2025/deid",
+        "gradient/16AUG2024": None,
+        "gradient/13JAN2025": None,
+        "segmed/batch1": "/mnt/efs/all-cxr/segmed/batch1",
+        "segmed/batch2": "/mnt/efs/all-cxr/segmed/batch2",
+        "segmed/batch3": "/mnt/efs/all-cxr/segmed/batch3",
+        "segmed/batch4": "/mnt/efs/all-cxr/segmed/batch4",
+        "simonmed": "/mnt/efs/all-cxr/simonmed/images/422ca224-a9f2-4c64-bf7c-bb122ae2a7bb"
+    }
+
+    args = argparse.Namespace(input_csv_path=INPUT_CSV_PATH,
+                              projections_csv_path=PROJECTIONS_CSV_PATH,
+                              output_file_path=OUTPUT_FILE_PATH,
+                              base_path_substitutions=BASE_PATH_SUBSTITUTIONS)
+
+    main(args)
