1053427208a7bdf1beff70ae70ddc1e5a374160d
Andrej Ikica <andrej@epsilonlabs.ai>
2025-06-16 21:16:55 +0000

    Added func for parsing labels.

--- old_repos/epsutils/epsutils/labels/labels_utils.py
+++ old_repos/epsutils/epsutils/labels/labels_utils.py
@@ -11,3 +11,18 @@ def to_multi_hot_encoding(labels, all_labels):
 
 def from_multi_hot_encoding(multi_hot_encoding, all_labels):
     return [all_labels[i] for i, val in enumerate(multi_hot_encoding) if val == 1]
+
+
+def parse_structured_labels(structured_labels, treat_uncertain_as_positive=True):
+    parsed_labels = {}
+    for item in structured_labels:
+        # Make sure body part is not repeated.
+        assert item["body_part"] not in parsed_labels
+
+        # Make sure no label is repeated to avoid scenarios where one occurence of the label would be certain and the other one uncertain.
+        label_names = [label["label"] for label in item["labels"]]
+        assert len(label_names) == len(set(label_names))
+
+        parsed_labels[item["body_part"]] = [label["label"] for label in item["labels"] if treat_uncertain_as_positive or label["confidence"].strip().lower() == "certain"]
+
+    return parsed_labels
