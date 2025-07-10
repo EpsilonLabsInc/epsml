bb2865fb34bca2e2f6b2ebc9f134815376498f39
Andrej Ikica <andrej.ikica@gmail.com>
2025-07-10 22:40:49 +0200

    Added script for scannoing reports files.

--- old_repos/epsdatasets/epsdatasets/helpers/generic/scripts/scan_and_analyze_reports_file.py
+++ old_repos/epsdatasets/epsdatasets/helpers/generic/scripts/scan_and_analyze_reports_file.py
@@ -1,57 +1,69 @@
+import argparse
 import ast
-import os
-from io import BytesIO, StringIO
+from io import StringIO
+from pprint import pprint
 
 import pandas as pd
 from tqdm import tqdm
 
-from epsutils.dicom import dicom_utils
 from epsutils.gcs import gcs_utils
-from epsutils.image import image_utils
-
-GCS_REPORTS_FILE = "gs://epsilonlabs-filestore/cleaned_CRs/GRADIENT_CR_batch_1.csv"
 
 
-def row_handler(row, index):
-    report_text = row["report_text"]
-    if "LUNG LESION" not in report_text.upper():
+def row_handler(*args):
+    row = args[0]
+    body_parts_to_include = args[2]
+    results = args[3]
+
+    if pd.isna(row["body_part_dicom"]):
         return
 
-    labels = [label.strip() for label in row["labels"].split(",")]
+    body_part = row["body_part_dicom"].lower()
+    body_part_found = False
 
-    image_paths_dict = ast.literal_eval(row["image_paths"])
-    image_paths = []
-    for value in image_paths_dict.values():
-        image_paths.extend(value["paths"])
+    for body_part_to_include in body_parts_to_include:
+        if body_part_to_include in body_part:
+            body_part_found = True
+            body_part = body_part_to_include
+            break
 
-    for image_path in image_paths:
-        image_path = os.path.join("gs://epsilon-data-us-central1/GRADIENT-DATABASE/CR/22JUL2024", image_path)
-        gcs_data = gcs_utils.split_gcs_uri(image_path)
-        content = gcs_utils.download_file_as_bytes(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
-        image = dicom_utils.get_dicom_image(BytesIO(content), custom_windowing_parameters={"window_center": 0, "window_width": 0})
-        image = image_utils.numpy_array_to_pil_image(image)
-        image.save(f"{os.path.basename(image_path)}.png")
+    if not body_part_found:
+        return
 
-    print("------------------------------------------")
-    print("Report text:")
-    print(report_text)
-    print("Labels:")
-    print(labels)
-    key = input("Press any key to continue...")
+    structured_labels = ast.literal_eval(row["structured_labels"])
+    labels = [sub_item["label"].lower() for item in structured_labels for sub_item in item["labels"] if item["body_part"] in ["Extremities", "Unknown"]]
+    labels = set(labels)
+
+    if body_part not in results:
+        results[body_part] = labels
+    else:
+        results[body_part].update(labels)
 
 
-def main():
-    print(f"Downloading reports file {GCS_REPORTS_FILE}")
-    gcs_data = gcs_utils.split_gcs_uri(GCS_REPORTS_FILE)
-    content = gcs_utils.download_file_as_string(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"])
+def main(args):
+    if gcs_utils.is_gcs_uri(args.reports_file):
+        print(f"Downloading reports file {args.reports_file}")
+        gcs_data = gcs_utils.split_gcs_uri(args.reports_file)
+        content = StringIO(gcs_utils.download_file_as_string(gcs_bucket_name=gcs_data["gcs_bucket_name"], gcs_file_name=gcs_data["gcs_path"]))
+    else:
+        content = args.reports_file
 
     print("Loading reports file")
-    df = pd.read_csv(StringIO(content))
+    df = pd.read_csv(content, low_memory=False)
 
     print("Reading reports file")
-    for index, row in tqdm(df.iterrows(), total=len(df)):
-        row_handler(row, index)
+    results = {}
+    for index, row in tqdm(df.iterrows(), total=len(df), desc="Processing"):
+        row_handler(row, index, args.body_parts_to_include, results)
+
+    print("Results:")
+    pprint(results, indent=4)
 
 
 if __name__ == "__main__":
-    main()
+    REPORTS_FILE = r"C:\Users\Andrej\Desktop\gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_all.csv"  # Can be local file or GCS URI.
+    BODY_PARTS_TO_INCLUDE = ["leg", "arm", "shoulder", "hand", "wrist", "elbow", "knee", "ankle", "foot", "hip"]
+
+    args = argparse.Namespace(reports_file=REPORTS_FILE,
+                              body_parts_to_include=BODY_PARTS_TO_INCLUDE)
+
+    main(args)
