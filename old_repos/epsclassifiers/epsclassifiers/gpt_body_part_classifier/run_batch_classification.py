94bb214672fa418d4bca4ad0c98047a9d40be3f6
Andrej Ikica <andrej@epsilonlabs.ai>
2025-07-15 22:40:29 +0000

    Refactoring.

--- old_repos/epsclassifiers/epsclassifiers/gpt_body_part_classifier/run_batch_classification.py
+++ old_repos/epsclassifiers/epsclassifiers/gpt_body_part_classifier/run_batch_classification.py
@@ -2,6 +2,7 @@ import argparse
 import ast
 import json
 import os
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 from io import StringIO, BytesIO
 
 import pandas as pd
@@ -24,55 +25,141 @@ def main(args):
     print("Loading reports file")
     df = pd.read_csv(content, low_memory=False)
 
-    print("Reading reports file")
-    request_id = 0
-    for _, row in tqdm(df.iterrows(), total=len(df), desc="Processing"):
-        base_path = row["base_path"]
-        if base_path not in args.base_path_substitutions:
-            raise ValueError(f"Base path '{base_path}' not in base path substitutions")
-        elif args.base_path_substitutions[base_path] is None:
-            continue
-        subst = args.base_path_substitutions[base_path]
+    print("Filtering reports")
+    filtered_reports = filter_reports(df=df,
+                                      base_path_substitutions=args.base_path_substitutions,
+                                      target_dicom_body_parts=args.target_dicom_body_parts,
+                                      target_image_size=args.target_image_size,
+                                      max_num_rows=args.max_num_rows)
 
-        dicom_body_part = row["body_part_dicom"].lower() if pd.notna(row["body_part_dicom"]) else ""
-        if args.dicom_body_part not in dicom_body_part:
-            continue
+    print("Running batches")
+    input_file_names, output_file_names = run_batches(filtered_reports=filtered_reports, gpt_prompt=args.gpt_prompt, gpt_config=args.gpt_config)
 
-        images = []
-        for image_path in ast.literal_eval(row["image_paths"]):
-            image_path = os.path.join(subst, image_path)
-            image = dicom_utils.get_dicom_image_fail_safe(image_path, custom_windowing_parameters={"window_center": 0, "window_width": 0})
-            image = image_utils.numpy_array_to_pil_image(image, convert_to_uint8=True, convert_to_rgb=True)
-            image = image.resize(args.target_image_size)
+    print("Assemble results")
+    results = assemble_results(output_file_names)
 
-            buffer = BytesIO()
-            image.save(buffer, format="JPEG")
-            buffer.seek(0)
-            images.append(buffer)
+    print("Updating reports")
+    mapping = {item["index"]: item["result"] for item in results}
+    df[args.column_name_to_add] = pd.Series(mapping).reindex(df.index)
 
-        request = gpt_utils.create_request(prompt="Which body part is in these x-ray images?",
-                                           images=images,
-                                           request_id=str(request_id),
-                                           deployment=args.gpt_config["batch_deployment"])
+    print("Saving output file")
+    df.to_csv(args.output_file, index=False)
 
-        content = json.dumps(request, ensure_ascii=False) + "\n"
+    if args.clean_up_files:
+        print("Cleaning up files")
+        files_to_delete = input_file_names + output_file_names
+        for file_to_delete in files_to_delete:
+            if os.path.exists(file_to_delete):
+                os.remove(file_to_delete)
 
-        content = gpt_utils.run_batch(input_jsonl=content,
-                                      endpoint=args.gpt_config["endpoint"],
-                                      api_key=args.gpt_config["api_key"],
-                                      api_version=args.gpt_config["api_version"],
-                                      is_content=True)
+    if args.gpt_config["clean_up_azure_files"]:
+        print("Cleaning up Azure files")
+        gpt_utils.delete_files(args.gpt_config["endpoint"],
+                               args.gpt_config["api_key"],
+                               args.gpt_config["api_version"])
 
-        # TODO: Remove.
-        print(content)
-        exit()
 
-        request_id += 1
+def process_row(row, base_path_substitutions, target_dicom_body_parts, target_image_size):
+    # Get base path substitution.
+    base_path = row.base_path
+    if base_path not in base_path_substitutions:
+        return None
+    elif base_path_substitutions[base_path] is None:
+        return None
+    subst = base_path_substitutions[base_path]
+
+    # Check body part DICOM tag. If it differs from target body parts, skip the row.
+    dicom_body_part = row.body_part_dicom.lower() if pd.notna(row.body_part_dicom) else ""
+    if not any(item in dicom_body_part for item in target_dicom_body_parts):
+        return None
+
+    # Get all study images and convert them to JPEG.
+    images = []
+    for image_path in ast.literal_eval(row.image_paths):
+        image_path = os.path.join(subst, image_path)
+        image = dicom_utils.get_dicom_image_fail_safe(image_path, custom_windowing_parameters={"window_center": 0, "window_width": 0})
+        image = image_utils.numpy_array_to_pil_image(image, convert_to_uint8=True, convert_to_rgb=True)
+        image = image.resize(target_image_size)
+
+        buffer = BytesIO()
+        image.save(buffer, format="JPEG")
+        buffer.seek(0)
+        images.append(buffer)
+
+    return {
+        "index": row.Index,
+        "images": images,
+        "report_text": row.report_text,
+        "body_part_dicom": row.body_part_dicom
+    }
+
+
+def filter_reports(df, base_path_substitutions, target_dicom_body_parts, target_image_size, max_num_rows):
+    with ThreadPoolExecutor() as executor:
+        results = list(tqdm(executor.map(lambda row: process_row(row, base_path_substitutions, target_dicom_body_parts, target_image_size), [row for row in df.itertuples()]),
+                            total=len(df),
+                            desc="Processing"))
+
+    filtered_reports = [item for item in results if item is not None]
+
+    return filtered_reports
+
+
+def run_batches(filtered_reports, gpt_prompt, gpt_config):
+    requests = []
+
+    for item in filtered_reports:
+        request = gpt_utils.create_request(system_prompt=gpt_prompt,
+                                           user_prompt=item["report_text"],
+                                           images=item["images"],
+                                           request_id=str(item["index"]),
+                                           deployment=gpt_config["batch_deployment"])
+        requests.append(request)
+
+    input_file_names = gpt_utils.save_requests_as_jsonl(requests=requests, file_name="input.jsonl")
+    output_file_names = [input_file_name.replace("input", "output") for input_file_name in input_file_names]
+
+    with ProcessPoolExecutor() as executor:
+        futures = []
+        for index, input_file_name in enumerate(input_file_names):
+            futures.append(executor.submit(
+                    gpt_utils.run_batch,
+                    input_file_name,
+                    output_file_names[index],
+                    gpt_config["endpoint"],
+                    gpt_config["api_key"],
+                    gpt_config["api_version"]
+                )
+            )
+
+        for future in as_completed(futures):
+            future.result()
+
+    return input_file_names, output_file_names
+
+
+def assemble_results(output_file_names):
+    results = []
+
+    for output_file_name in output_file_names:
+        with open(output_file_name, "r") as file:
+            for line in file:
+                data = json.loads(line)
+                index = data["custom_id"]
+                result = data["response"]["body"]["choices"][0]["message"]["content"].strip().strip("\"")
+                results.append({"index": index, "result": result})
+
+    return results
 
 
 if __name__ == "__main__":
     REPORTS_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_test.csv"  # Reports CSV file. Can be local file or GCS URI.
-    DICOM_BODY_PART = "arm"
+    OUTPUT_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_arm_segment_test.csv"
+    COLUMN_NAME_TO_ADD = "arm_segment"
+    TARGET_DICOM_BODY_PARTS = ["shoulder", "arm", "elbow", "hand", "palm", "finger"]
+    TARGET_IMAGE_SIZE = (200, 200)
+    MAX_NUM_ROWS = 11
+    CLEAN_UP_FILES = True
     BASE_PATH_SUBSTITUTIONS = {
         "gradient/22JUL2024": None,
         "gradient/20DEC2024": None,
@@ -85,19 +172,37 @@ if __name__ == "__main__":
         "segmed/batch4": "/mnt/sfs-segmed-34/segmed_4",
         "simonmed": "/mnt/sfs-simonmed"
     }
-    TARGET_IMAGE_SIZE = (200, 200)
+    GPT_PROMPT = """
+You are a medical imaging assistant tasked with identifying the body part shown in X-ray images and/or described
+in accompanying medical reports. Follow these strict guidelines:
+1. Prioritize visual data.
+   Always analyze the X-ray image first. Use the report only if the image is unclear, missing, or inconclusive.
+2. Categorize explicitly.
+   Respond with one of the following labels only: "Shoulder", "Arm", "Hand", or "Other". Include fingers under "Hand".
+   Include elbows under "Arm". Use "Other" if the body part is not one of the above or cannot be determined.
+3. Respond concisely.
+   Your entire output should be a single word: one of the four category labels.
+"""
     GPT_CONFIG = {
         "endpoint": "https://epsilon-eastus.openai.azure.com/",
         "api_key": "9b568fdffb144272811cb5fad8b584a0",
         "api_version": "2024-12-01-preview",
         "batch_deployment": "gpt-4.1",
-        "batch_mini_deployment": "gpt-4.1"
+        "batch_mini_deployment": "gpt-4.1",
+        "standard_deployment": "gpt-4o-standard",
+        "standard_mini_deployment": "gpt-4o-mini-standard",
+        "clean_up_azure_files": True
     }
 
     args = argparse.Namespace(reports_file=REPORTS_FILE,
-                              dicom_body_part=DICOM_BODY_PART,
-                              base_path_substitutions=BASE_PATH_SUBSTITUTIONS,
+                              output_file=OUTPUT_FILE,
+                              column_name_to_add=COLUMN_NAME_TO_ADD,
+                              target_dicom_body_parts=TARGET_DICOM_BODY_PARTS,
                               target_image_size=TARGET_IMAGE_SIZE,
+                              max_num_rows=MAX_NUM_ROWS,
+                              clean_up_files=CLEAN_UP_FILES,
+                              base_path_substitutions=BASE_PATH_SUBSTITUTIONS,
+                              gpt_prompt=GPT_PROMPT,
                               gpt_config=GPT_CONFIG)
 
     main(args)
