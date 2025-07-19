02a7b64a2a2abb4ae7b0799f07a0e3d91ba7776c
Andrej Ikica <andrej@epsilonlabs.ai>
2025-07-19 20:09:42 +0000

    Added new prompt.

--- old_repos/epsclassifiers/epsclassifiers/gpt_body_part_classifier/prompts.py
+++ old_repos/epsclassifiers/epsclassifiers/gpt_body_part_classifier/prompts.py
@@ -46,3 +46,18 @@ Your entire response must be a **single word**: the body part label only.
 """
 
 ALL_EXTREMITY_SEGMENTS_TARGET_BODY_PARTS = TargetBodyParts(read_location=ReadLocation.REPORT, values=["extremities"])
+
+# Is spine?
+
+IS_SPINE_GPT_PROMPT = """
+You are a medical imaging assistant tasked with identifying the body part shown in X-ray images and/or described
+in accompanying medical reports. Follow these strict guidelines:
+1. Prioritize visual data.
+   Always analyze the X-ray image first. Use the report only if the image is unclear, missing, or inconclusive.
+2. Categorize explicitly.
+   Respond with one of the following labels only: "Spine" or "Other". Use "Spine" if the body part is spine and "Other" if the body part is not spine.
+3. Respond concisely.
+   Your entire output should be a single word, either "Spine" or "Other".
+"""
+
+IS_SPINE_TARGET_BODY_PARTS = TargetBodyParts(read_location=ReadLocation.REPORT, values=["extremities"])
