3de4b076a7bfb091993f76e2378f8963d48eff3d
Andrej Ikica <andrej@epsilonlabs.ai>
2025-07-20 07:24:02 +0000

    Supported content list.

--- old_repos/epsutils/epsutils/csv/scripts/filter_csv_file.py
+++ old_repos/epsutils/epsutils/csv/scripts/filter_csv_file.py
@@ -7,10 +7,17 @@ def main(args):
     print(f"Loading {args.input_csv_file}")
     df = pd.read_csv(args.input_csv_file, low_memory=False)
 
+    values = df[args.column_name_for_filtering].unique()
+    print("Unique values in the column for filtering:")
+    print(values)
+
     print(f"Number of rows in the dataset BEFORE filtering: {len(df)}")
 
     print("Filtering...")
-    df = df[df[args.column_name_for_filtering] == args.content_for_filtering]
+    if isinstance(args.content_for_filtering, list):
+        df = df[df[args.column_name_for_filtering].isin(args.content_for_filtering)]
+    else:
+        df = df[df[args.column_name_for_filtering] == args.content_for_filtering]
 
     print(f"Number of rows in the dataset AFTER filtering: {len(df)}")
 
@@ -19,10 +26,10 @@ def main(args):
 
 
 if __name__ == "__main__":
-    INPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_arm_segment_val.csv"
-    OUTPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_arm_segment_hand_val.csv"
-    COLUMN_NAME_FOR_FILTERING = "arm_segment"
-    CONTENT_FOR_FILTERING = "Hand"
+    INPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_all_extremity_segments_train.csv"
+    OUTPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_all_extremity_segments_filtered_train.csv"
+    COLUMN_NAME_FOR_FILTERING = "all_extremity_segments"
+    CONTENT_FOR_FILTERING = ["Arm", "Hand", "Shoulder", "Leg", "Foot", "Ankle", "Knee", "Other"]  # Can be a single value or a list of values.
 
     args = argparse.Namespace(input_csv_file=INPUT_CSV_FILE,
                               output_csv_file=OUTPUT_CSV_FILE,
