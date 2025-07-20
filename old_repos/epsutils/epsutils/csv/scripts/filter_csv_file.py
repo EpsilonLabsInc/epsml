dce630a9d05fa67426b87e80e212cb718ef72e39
Andrej Ikica <andrej@epsilonlabs.ai>
2025-07-20 08:48:36 +0000

    Added more log messages.

--- old_repos/epsutils/epsutils/csv/scripts/filter_csv_file.py
+++ old_repos/epsutils/epsutils/csv/scripts/filter_csv_file.py
@@ -8,7 +8,7 @@ def main(args):
     df = pd.read_csv(args.input_csv_file, low_memory=False)
 
     values = df[args.column_name_for_filtering].unique()
-    print("Unique values in the column for filtering:")
+    print("Unique values in the column for filtering BEFORE filtering:")
     print(values)
 
     print(f"Number of rows in the dataset BEFORE filtering: {len(df)}")
@@ -19,6 +19,10 @@ def main(args):
     else:
         df = df[df[args.column_name_for_filtering] == args.content_for_filtering]
 
+    values = df[args.column_name_for_filtering].unique()
+    print("Unique values in the column for filtering AFTER filtering:")
+    print(values)
+
     print(f"Number of rows in the dataset AFTER filtering: {len(df)}")
 
     print(f"Saving filtered dataset to {args.output_csv_file}")
@@ -26,8 +30,8 @@ def main(args):
 
 
 if __name__ == "__main__":
-    INPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_all_extremity_segments_train.csv"
-    OUTPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_all_extremity_segments_filtered_train.csv"
+    INPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_all_extremity_segments_val.csv"
+    OUTPUT_CSV_FILE = "/mnt/training/splits/gradient_batches_1-5_segmed_batches_1-4_simonmed_batches_1-10_reports_with_labels_with_all_extremity_segments_filtered_val.csv"
     COLUMN_NAME_FOR_FILTERING = "all_extremity_segments"
     CONTENT_FOR_FILTERING = ["Arm", "Hand", "Shoulder", "Leg", "Foot", "Ankle", "Knee", "Other"]  # Can be a single value or a list of values.
 
