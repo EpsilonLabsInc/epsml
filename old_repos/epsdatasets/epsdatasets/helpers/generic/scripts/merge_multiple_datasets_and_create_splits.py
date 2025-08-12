3e78eed041fbff51f3834683f424efc890705338
Andrej Ikica <andrej@epsilonlabs.ai>
2025-08-12 19:23:50 +0000

    Updated paths.

--- old_repos/epsdatasets/epsdatasets/helpers/generic/scripts/merge_multiple_datasets_and_create_splits.py
+++ old_repos/epsdatasets/epsdatasets/helpers/generic/scripts/merge_multiple_datasets_and_create_splits.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     DATASETS_INFO = [
         {
             "dataset_name": "gradient",
-            "file_name": "/mnt/sfs-gradient-chest/GRADIENT_CR_ALL_BATCHES_cleaned_standardized_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/gradient/gradient_cr_all_batches_final.csv",
             "images_base_path": "gradient",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_1",
-            "file_name": "/mnt/sfs-segmed-1/reports/segmed_batch_1_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_1_final.csv",
             "images_base_path": "segmed/batch1",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -142,7 +142,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_2",
-            "file_name": "/mnt/sfs-segmed-2/reports/segmed_batch_2_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_2_final.csv",
             "images_base_path": "segmed/batch2",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -150,7 +150,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_3",
-            "file_name": "/mnt/sfs-segmed-34/segmed_3/reports/segmed_batch_3_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_3_final.csv",
             "images_base_path": "segmed/batch3",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -158,7 +158,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_4",
-            "file_name": "/mnt/sfs-segmed-34/segmed_4/reports/segmed_batch_4_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_4_final.csv",
             "images_base_path": "segmed/batch4",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -166,7 +166,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_5",
-            "file_name": "/mnt/sfs-segmed567/segmed_5/reports/segmed_batch_5_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_5_final.csv",
             "images_base_path": "segmed/batch5",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -174,7 +174,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_6",
-            "file_name": "/mnt/sfs-segmed567/segmed_6/reports/segmed_batch_6_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_6_final.csv",
             "images_base_path": "segmed/batch6",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -182,7 +182,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_7",
-            "file_name": "/mnt/sfs-segmed567/segmed_7/reports/segmed_batch_7_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_7_final.csv",
             "images_base_path": "segmed/batch7",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -190,7 +190,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_8",
-            "file_name": "/mnt/misc/segmed/batch8/reports/segmed_batch_8_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_8_final.csv",
             "images_base_path": "segmed/batch8",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -198,7 +198,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "segmed_batch_9",
-            "file_name": "/mnt/misc/segmed/batch9/reports/segmed_batch_9_merged_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/segmed/batch1/segmed_batch_9_final.csv",
             "images_base_path": "segmed/batch9",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -206,7 +206,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_1",
-            "file_name": "/mnt/sfs-simonmed/reports/batch1/simonmed_batch_1_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_1_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -214,7 +214,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_2",
-            "file_name": "/mnt/sfs-simonmed/reports/batch2/simonmed_batch_2_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_2_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -222,7 +222,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_3",
-            "file_name": "/mnt/sfs-simonmed/reports/batch3/simonmed_batch_3_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_3_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -230,7 +230,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_4",
-            "file_name": "/mnt/sfs-simonmed/reports/batch4/simonmed_batch_4_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_4_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -238,7 +238,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_5",
-            "file_name": "/mnt/sfs-simonmed/reports/batch5/simonmed_batch_5_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_5_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -246,7 +246,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_6",
-            "file_name": "/mnt/sfs-simonmed/reports/batch6/simonmed_batch_6_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_6_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -254,7 +254,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_7",
-            "file_name": "/mnt/sfs-simonmed/reports/batch7/simonmed_batch_7_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_7_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -262,7 +262,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_8",
-            "file_name": "/mnt/sfs-simonmed/reports/batch8/simonmed_batch_8_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_8_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
@@ -270,7 +270,7 @@ if __name__ == "__main__":
         },
         {
             "dataset_name": "simonmed_batch_9",
-            "file_name": "/mnt/sfs-simonmed/reports/batch9/simonmed_batch_9_reports_with_image_paths_filtered_standardized_with_dicom_data_mapped_modalities_mapped_body_parts_with_uncertain_labels_cleaned_unflagged.csv",
+            "file_name": "/mnt/all-data/reports/simonmed/batch1/simonmed_batch_9_final.csv",
             "images_base_path": "simonmed",
             "report_text_column": "cleaned_report_text",
             "labels_column": "labels",
