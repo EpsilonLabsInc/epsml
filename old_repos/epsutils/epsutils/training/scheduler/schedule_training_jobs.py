aebe7f2146e1c4ae76f9625d267d6a93e28173fc
Andrej Ikica <andrej@epsilonlabs.ai>
2025-09-25 14:46:35 +0000

    Implemented training scheduler.

---
+++ old_repos/epsutils/epsutils/training/scheduler/schedule_training_jobs.py
@@ -0,0 +1,147 @@
+import argparse
+import json
+import os
+import yaml
+from pathlib import Path
+from typing import List, Tuple
+
+from epsutils.training.scheduler.training_job_analyzer import TrainingJobAnalyzer
+
+EXPECTED_BATCH_SIZE = 128
+EXPECTED_BATCH_TIME_IN_SEC = 10
+
+
+def get_training_jobs(training_jobs_file_or_config_path):
+    path = Path(training_jobs_file_or_config_path)
+
+    if path.is_file() and path.suffix.lower() == ".json":
+        print("Loading training jobs")
+        with open(path, "r") as file:
+            training_jobs = json.load(file)
+
+    elif path.is_dir():
+        print("Searching for config files and analyzing training jobs, this might take a while...")
+        training_jobs = TrainingJobAnalyzer().find_config_files_and_get_training_jobs(path)
+
+    else:
+        raise ValueError("training_jobs_file_or_config_path must be either .json file or path to the config files")
+
+    return training_jobs
+
+
+def assign_training_jobs_to_buckets(training_jobs: List, num_buckets: int, min_epochs: int = 2, max_epochs: int = 10) -> List[List[Tuple[str, int]]]:
+    print(f"Assigning {len(training_jobs)} training jobs to {num_buckets} buckets")
+
+    assert any(job["training_dataset_size"] is not None for job in training_jobs)
+
+    max_count = max(job["training_dataset_size"] for job in training_jobs if job["training_dataset_size"] is not None)
+    max_total_count = min_epochs * max_count
+
+    print("Determining number of epochs for each training job using the following values:")
+    print(f"+ Max count: {max_count}")
+    print(f"+ Max total count: {max_total_count}")
+    print(f"+ Min epochs: {min_epochs}")
+    print(f"+ Max epochs: {max_epochs}")
+
+    total_counts = []
+    for job in training_jobs:
+        config_file = job["config_file"]
+        count = job["training_dataset_size"]
+
+        if count is None:
+            continue
+
+        num_epochs = max_total_count // count
+        num_epochs = max(min(num_epochs, max_epochs), min_epochs)
+        total_count = count * num_epochs
+        total_counts.append({"config_file": config_file, "count": count, "num_epochs": num_epochs, "total_count": total_count})
+
+    total_counts.sort(key=lambda x: x["total_count"], reverse=True)
+
+    buckets = [[] for _ in range(num_buckets)]
+    bucket_sums = [0] * num_buckets
+
+    for item in total_counts:
+        min_bucket_idx = bucket_sums.index(min(bucket_sums))
+        buckets[min_bucket_idx].append(item)
+        bucket_sums[min_bucket_idx] += item["total_count"]
+
+    return buckets, bucket_sums
+
+
+def update_num_epochs(input_config_file, output_config_file, num_epochs):
+    with open(input_config_file, "r") as file:
+        config = yaml.safe_load(file)
+
+    config["training"]["num_epochs"] = num_epochs
+
+    with open(output_config_file, "w") as file:
+        yaml.dump(config, file)
+
+
+def copy_config_files(buckets, output_dir):
+    print("Copying config files into buckets")
+
+    for index, bucket in enumerate(buckets):
+        bucket_dir = os.path.join(output_dir, f"bucket_{index + 1}")
+        os.makedirs(bucket_dir, exist_ok=True)
+
+        for item in bucket:
+            update_num_epochs(input_config_file=item["config_file"],
+                              output_config_file=os.path.join(bucket_dir, os.path.basename(item["config_file"])),
+                              num_epochs=item["num_epochs"])
+
+
+def generate_summary(buckets, bucket_sums):
+    total_cout = sum(bucket_sums)
+    min_count = min(bucket_sums)
+    max_count = max(bucket_sums)
+
+    expected_times = []
+    for bucket_sum in bucket_sums:
+        expected_time = (bucket_sum / EXPECTED_BATCH_SIZE) * EXPECTED_BATCH_TIME_IN_SEC / 3600
+        expected_time = round(expected_time, 2)
+        expected_times.append(expected_time)
+
+    min_time = min(expected_times)
+    max_time = max(expected_times)
+
+    print("")
+    print("Summary:")
+    print(f"+ Buckets created: {len(buckets)}")
+    print(f"+ Total count over all buckets: {total_cout:,}")
+    print(f"+ Min count: {min_count:,}")
+    print(f"+ Max count: {max_count:,}")
+    print(f"+ Training jobs distribution: {[len(bucket) for bucket in buckets]}")
+    print(f"+ Expected training times (h): {expected_times}")
+    print(f"+ Min expected training time (h): {min_time}")
+    print(f"+ Max expected training time (h): {max_time}")
+
+
+def main(args):
+    training_jobs = get_training_jobs(args.training_jobs_file_or_config_path)
+
+    buckets, bucket_sums = assign_training_jobs_to_buckets(training_jobs=training_jobs,
+                                                           num_buckets=args.num_buckets,
+                                                           min_epochs=args.min_epochs,
+                                                           max_epochs=args.max_epochs)
+
+    copy_config_files(buckets=buckets, output_dir=args.output_dir)
+
+    generate_summary(buckets=buckets, bucket_sums=bucket_sums)
+
+
+if __name__ == "__main__":
+    TRAINING_JOBS_FILE_OR_CONFIG_PATH = "training_jobs.json"
+    NUM_BUCKETS = 45
+    MIN_EPOCHS = 2
+    MAX_EPOCHS = 10
+    OUTPUT_DIR = "/mnt/training/v2.0.0/buckets"
+
+    args = argparse.Namespace(training_jobs_file_or_config_path=TRAINING_JOBS_FILE_OR_CONFIG_PATH,
+                              num_buckets=NUM_BUCKETS,
+                              min_epochs=MIN_EPOCHS,
+                              max_epochs=MAX_EPOCHS,
+                              output_dir=OUTPUT_DIR)
+
+    main(args)
--- old_repos/epsutils/epsutils/training/scheduler/training_job_analyzer.py
+++ old_repos/epsutils/epsutils/training/scheduler/training_job_analyzer.py
@@ -9,14 +9,14 @@ from epsdatasets.helpers.generic.generic_dataset_helper import GenericDatasetHel
 from epsutils.training.config.config_loader import ConfigLoader
 
 
-class DataAnalyzer:
+class TrainingJobAnalyzer:
     def __init__(self, ignore_templates=True, low_memory=False, simulation_mode=False, save_intermediate_results_step=None):
         self.__ignore_templates = ignore_templates
         self.__low_memory = low_memory
         self.__simulation_mode = simulation_mode
         self.__save_intermediate_results_step = save_intermediate_results_step
 
-    def find_config_files_and_get_num_training_samples(self, root_config_path):
+    def find_config_files_and_get_training_jobs(self, root_config_path):
         config_files = list(Path(root_config_path).rglob("*.yaml"))
         config_files = [os.path.abspath(config_file) for config_file in config_files]
 
@@ -26,11 +26,11 @@ class DataAnalyzer:
         print(f"Found the following {len(config_files)} config files:")
         print(json.dumps(config_files, indent=4))
 
-        return self.get_num_training_samples(config_files)
+        return self.get_training_jobs(config_files)
 
-    def get_num_training_samples(self, config_files):
+    def get_training_jobs(self, config_files):
         if len(config_files) == 0:
-            return {}
+            return []
 
         # Prefetch the data.
         if not self.__simulation_mode:
@@ -40,13 +40,13 @@ class DataAnalyzer:
             print(f"Loading {train_file}")
             train_df = pd.read_csv(train_file, low_memory=self.__low_memory)
 
-        num_samples_dict = {}
+        training_jobs = []
 
         for index, config_file in enumerate(config_files):
             # Save intermediate results.
             if self.__save_intermediate_results_step is not None and index % self.__save_intermediate_results_step == 0 and index > 0:
-                with open(f"num_training_samples_intermediate_{index}.json", "w") as f:
-                    json.dump(num_samples_dict, f, indent=4)
+                with open(f"training_jobs_intermediate_{index}.json", "w") as f:
+                    json.dump(training_jobs, f, indent=4)
 
             # Generate status message.
             message = f"{index + 1}/{len(config_files)} Config file: {config_file}"
@@ -61,7 +61,11 @@ class DataAnalyzer:
 
             # Use a random number in simulation mode.
             if self.__simulation_mode:
-                num_samples_dict[config_file] = None if random.random() < 0.1 else random.randint(10, 200000)
+                training_jobs.append({
+                    "job_id": index,
+                    "config_file": config_file,
+                    "training_dataset_size": None if random.random() < 0.1 else random.randint(10, 200000)
+                })
                 continue
 
             # Load config.
@@ -99,10 +103,14 @@ class DataAnalyzer:
                 print(e)
                 dataset_helper = None
 
-            # Get num training samples.
-            num_samples_dict[config_file] = len(dataset_helper.get_torch_train_dataset()) if dataset_helper is not None else None
+            # Add training job.
+            training_jobs.append({
+                "job_id": index,
+                "config_file": config_file,
+                "training_dataset_size": len(dataset_helper.get_torch_train_dataset()) if dataset_helper is not None else None
+            })
 
-        return num_samples_dict
+        return training_jobs
 
 
 if __name__ == "__main__":
@@ -112,15 +120,15 @@ if __name__ == "__main__":
     SAVE_INTERMEDIATE_RESULTS_STEP = None
     ROOT_CONFIG_PATH = "../config/configs/v2_0_0"
 
-    num_training_samples = DataAnalyzer(ignore_templates=IGNORE_TEMPLATES,
+    training_jobs = TrainingJobAnalyzer(ignore_templates=IGNORE_TEMPLATES,
                                         low_memory=LOW_MEMORY,
                                         simulation_mode=SIMULATION_MODE,
-                                        save_intermediate_results_step=SAVE_INTERMEDIATE_RESULTS_STEP).find_config_files_and_get_num_training_samples(ROOT_CONFIG_PATH)
+                                        save_intermediate_results_step=SAVE_INTERMEDIATE_RESULTS_STEP).find_config_files_and_get_training_jobs(ROOT_CONFIG_PATH)
 
-    print("Num training samples:")
-    print(json.dumps(num_training_samples, indent=4))
+    print("Training jobs:")
+    print(json.dumps(training_jobs, indent=4))
 
     print("")
-    print("Saving results to file")
-    with open("num_training_samples.json", "w") as f:
-        json.dump(num_training_samples, f, indent=4)
+    print("Saving training jobs to file")
+    with open("training_jobs.json", "w") as f:
+        json.dump(training_jobs, f, indent=4)
